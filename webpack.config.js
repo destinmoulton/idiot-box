44d400f90d62ce33b986dd0a12319300372f6060
Destin Moulton <destin.moulton@gmail.com>
2017-06-22 15:20:15 -0600

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/client/App.js',
+    output: { 
+        path: path.join(__dirname, 'dist/server/public/js'), 
+        filename: 'idiot-box.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react'],
+                    plugins: ["transform-class-properties", ["import", { libraryName: "antd" }]]
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({ // <-- key to reducing React's size
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    ]
+};
