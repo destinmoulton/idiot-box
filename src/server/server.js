0bd778d0dc3e16bd0b8a509b4f29068da53d7ea2
Destin Moulton <destin.moulton@gmail.com>
2017-06-28 08:43:08 -0600

    Modified the base route to use * for all react routes.

--- src/server/server.js
+++ src/server/server.js
@@ -1,33 +1,35 @@
 import path from 'path';
 import express from 'express';
 
-import db from 'sqlite';
-
-import logger from './logger';
-
 const app = express();
 
-const PORT = 3000;
-const DB_PATH = path.resolve(__dirname, '../database/idiot-box.sqlite3');
+import ibdb from './db/IBDB';
+import { setupSocketIO } from './socket.io/io';
+import logger from './logger';
 
+import dbconfig from './config/db';
+
+const PORT = 3000;
 const PUBLIC_PATH = path.resolve(__dirname, '../public');
 
 app.use(express.static(PUBLIC_PATH));
 
-app.get('/', (req, res)=>{
+app.get('*', (req, res)=>{
     res.sendFile(path.join(PUBLIC_PATH, '/index.html'));
 });
 
 Promise.resolve()
     .then(()=>{
-        db.open(DB_PATH)
+        ibdb.connect(dbconfig);
     })
     .then(()=>{
         app.listen(PORT, ()=>{
             console.log("\n---------------------------------------");
             console.log("Idiot Box Server running on Port "+PORT);
             console.log("---------------------------------------");
+            setupSocketIO(app);
         });
+
     })
     .catch((err)=>{
         logger.error(err);
