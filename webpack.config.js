65cd4f24a776a965fda938479c4a13d2ac5497f1
Destin Moulton <destin.moulton@gmail.com>
2017-06-28 10:11:53 -0600

    Added transform-object-rest-spread preset to babel.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
-                    presets: ['es2015', 'react'],
+                    presets: ['es2015', 'react', 'transform-object-rest-spread'],
                     plugins: ["transform-class-properties", ["import", { libraryName: "antd" }]]
                 }
             }
