62f5c7e4a5d096fdac4671860d487b155c8f389b
Destin Moulton <destin.moulton@gmail.com>
2017-06-29 16:19:45 -0600

    Added redux action types.

---
+++ src/client/actions/actionTypes.js
@@ -0,0 +1,19 @@
+// Define the constant action types for the redux actions and reducers
+// Use ALL_CAPS
+
+export const FS_DIR_RECEIVED = 'FS_DIR_RECEIVED';
+
+
+// General Socket IO Events
+export const IO_EMIT_SETUP = 'IO_EMIT_SETUP';
+export const IO_EMIT_SUCCESS = 'IO_EMIT_SUCCESS';
+export const IO_EMIT_FAILURE = 'IO_EMIT_FAILURE';
+export const IO_ON_SETUP = 'IO_ON_SETUP';
+export const IO_ON_SUCCESS = 'IO_ON_SUCCESS';
+export const IO_ON_FAILURE = 'IO_ON_FAILURE';
+
+// server events
+export const SRV_CONNECT_RECEIVED = 'SRV_CONNECT_RECEIVED'
+export const SRV_DISCONNECT_RECEIVED  = 'SRV_DISCONNECT_RECEIVED';
+export const SRV_INFO_RECEIVED = 'SRV_INFO_RECEIVED';
+
