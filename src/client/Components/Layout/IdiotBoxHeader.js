9dbaebd824b571ec6ef70523136054322f494a0d
Destin Moulton <destin.moulton@gmail.com>
2017-06-29 15:49:10 -0600

    Moved header menu to IdiotBoxHeader.

---
+++ src/client/Components/Layout/IdiotBoxHeader.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+const { Header } = Layout;
+
+class IdiotBoxHeader extends Component {
+    render() {
+        return (
+            <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="browse">
+                        <Link to="/browse">New Episodes</Link>
+                    </Menu.Item>
+                    <Menu.Item key="shows">
+                        <Link to="/shows">Shows</Link>
+                    </Menu.Item>
+                    <Menu.Item key="movies">
+                        <Link to="/movies">Movies</Link>
+                    </Menu.Item>
+                    <Menu.Item key="settings">
+                        <Link to="/settings">Settings</Link>
+                    </Menu.Item>
+                </Menu>
+            </Header>
+        );
+    }
+}
+
+export default IdiotBoxHeader;
