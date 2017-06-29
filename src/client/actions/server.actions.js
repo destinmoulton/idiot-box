55107d3908eac2e862780de7ecc40795b8dfdf36
Destin Moulton <destin.moulton@gmail.com>
2017-06-29 16:16:49 -0600

    Added server actions.

---
+++ src/client/actions/server.actions.js
@@ -0,0 +1,86 @@
+import { socketClient } from '../store';
+
+import {
+    IO_EMIT_SETUP,
+    IO_EMIT_SUCCESS,
+    IO_EMIT_FAILURE,
+    IO_ON_SETUP,
+    IO_ON_SUCCESS,
+    IO_ON_FAILURE,
+
+    SRV_CONNECT_RECEIVED,
+    SRV_DISCONNECT_RECEIVED,
+    SRV_INFO_RECEIVED,
+} from './actionTypes';
+
+export function srvConnect(){
+    return (dispatch)=>{
+        return socketClient.connect()
+            .then(()=>{
+                dispatch(waitforDisconnectReception(dispatch));
+                dispatch(srvConnectionReceived());
+                dispatch(srvGetServerInfo());
+            });
+    }
+}
+
+function waitforDisconnectReception(dispatch){
+    return {
+        type: 'socket',
+        types: [IO_ON_SETUP, IO_ON_SUCCESS, IO_ON_FAILURE],
+        promise: (socket) => {
+            return socket.on('disconnect', ()=>{
+                dispatch(srvDisconnectReceived());
+            });
+        }
+    }
+}
+
+function srvConnectionReceived(){
+    return {
+        type: SRV_CONNECT_RECEIVED
+    }
+}
+
+function srvDisconnectReceived(){
+    return {
+        type: SRV_DISCONNECT_RECEIVED
+    }
+}
+
+export function srvGetServerInfo(){
+    return (dispatch)=>{
+        dispatch(waitforInfoReception(dispatch));
+        dispatch(emitInfoRequest());
+    }
+}
+
+function waitforInfoReception(dispatch){
+    return {
+        type: 'socket',
+        types: [IO_ON_SETUP, IO_ON_SUCCESS, IO_ON_FAILURE],
+        promise: (socket) => {
+            socket.off('server.info.request');
+            return socket.on('filesystem.info.ready', (recd)=>{
+                dispatch(srvInfoReceived(recd));
+            });
+        }
+    }
+}
+
+function emitInfoRequest(){
+    return {
+        type: 'socket',
+        types: [IO_EMIT_SETUP, IO_EMIT_SUCCESS, IO_EMIT_FAILURE],
+        promise: (socket) => {
+            return socket.emit('server.info.request');
+        }
+    }
+}
+
+function srvInfoReceived(serverInfo){
+    return {
+        type: SRV_INFO_RECEIVED,
+        serverInfo
+    }
+}
