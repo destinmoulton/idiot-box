72897cebf698be9fed2a4b0837c071a83ed6b843
Destin Moulton <destin.moulton@gmail.com>
2017-07-03 11:49:04 -0600

    Added socket io methods for getting, adding, and updating settings using the SettingsModel.

--- dist/server/app/socket.io/settings.io.js
+++ dist/server/app/socket.io/settings.io.js
@@ -5,6 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = settingsIOListeners;
 
+var _error = require('../error');
+
+var _error2 = _interopRequireDefault(_error);
+
+var _IBDB = require('../db/IBDB');
+
+var _IBDB2 = _interopRequireDefault(_IBDB);
+
 var _io = require('./io');
 
 var _io2 = _interopRequireDefault(_io);
@@ -13,10 +21,35 @@ var _logger = require('../logger');
 
 var _logger2 = _interopRequireDefault(_logger);
 
+var _SettingsModel = require('../db/SettingsModel');
+
+var _SettingsModel2 = _interopRequireDefault(_SettingsModel);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function settingsIOListeners(socket) {
-    socket.on('settings.test', function (msg) {
-        _logger2.default.debug(msg);
+    var settingsModel = new _SettingsModel2.default(_IBDB2.default);
+    socket.on('settings.get.category', function (options) {
+        settingsModel.getAllForCategory(options.category).then(function (settings) {
+            socket.emit('settings.data.category', settings);
+        }).catch(function (err) {
+            return (0, _error2.default)(err);
+        });
+    });
+
+    socket.on('settings.add.request', function (options) {
+        settingsModel.addSetting(options.category, options.category, options.value).then(function () {
+            socket.emit('settings.add.complete', { status: "success" });
+        }).catch(function (err) {
+            return (0, _error2.default)(err);
+        });
+    });
+
+    socket.on('settings.update.request', function (options) {
+        settingsModel.updateSetting(options.id, options.category, options.key, options.value).then(function () {
+            socket.emit('settings.update.complete', { status: "success" });
+        }).catch(function (err) {
+            return (0, _error2.default)(err);
+        });
     });
 }
--- src/server/socket.io/settings.io.js
+++ src/server/socket.io/settings.io.js
@@ -1,8 +1,33 @@
+import error from '../error';
+import ibdb from '../db/IBDB';
 import io from './io';
 import logger from '../logger';
 
+import SettingsModel from '../db/SettingsModel';
+
 export default function settingsIOListeners(socket){
-    socket.on('settings.test', (msg)=>{
-        logger.debug(msg);
+    const settingsModel = new SettingsModel(ibdb);
+    socket.on('settings.get.category', (options)=>{
+        settingsModel.getAllForCategory(options.category)
+            .then((settings)=>{
+                socket.emit('settings.data.category', settings);
+            })
+            .catch((err)=> error(err));
+    });
+
+    socket.on('settings.add.request', (options)=>{
+        settingsModel.addSetting(options.category, options.category, options.value)
+            .then(()=>{
+                socket.emit('settings.add.complete', {status:"success"});
+            })
+            .catch((err)=>error(err));
+    });
+
+    socket.on('settings.update.request', (options)=>{
+        settingsModel.updateSetting(options.id, options.category, options.key, options.value)
+            .then(()=>{
+                socket.emit('settings.update.complete', {status:"success"});
+            })
+            .catch((err)=>error(err));
     });
 }
