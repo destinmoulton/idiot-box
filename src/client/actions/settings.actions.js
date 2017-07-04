549398191bf816a7a85c593b68c4a89a91cb7c12
Destin Moulton <destin.moulton@gmail.com>
2017-07-04 10:58:48 -0600

    Modified settings actions to use new api.

--- src/client/actions/settings.actions.js
+++ src/client/actions/settings.actions.js
@@ -1,43 +1,16 @@
-import {
-    IO_EMIT_SETUP,
-    IO_EMIT_SUCCESS,
-    IO_EMIT_FAILURE,
-    IO_ON_SETUP,
-    IO_ON_SUCCESS,
-    IO_ON_FAILURE,
+import { emitAPIRequest } from './api.actions';
 
+import {
     SETTING_LIST_RECEIVED
 } from './actionTypes';
 
 export function getSettingsForCategory(category){
     return (dispatch)=>{
-        dispatch(emitGetSettingsRequest(category));
-        dispatch(waitforSettingsReception(dispatch, category));
-    }
-}
-
-function emitGetSettingsRequest(category){
-    const options = {
-        category
-    };
-    return {
-        type: 'socket',
-        types: [IO_EMIT_SETUP, IO_EMIT_SUCCESS, IO_EMIT_FAILURE],
-        promise: (socket) => {
-            return socket.emit('settings.get.category', options);
-        }
-    }
-}
-
-function waitforSettingsReception(dispatch, category){
-    return {
-        type: 'socket',
-        types: [IO_ON_SETUP, IO_ON_SUCCESS, IO_ON_FAILURE],
-        promise: (socket) => {
-            return socket.on('settings.data.category', (settings)=>{
-                dispatch(settingsReceived(category, settings));
-            });
-        }
+        const endpoint = 'settings.category.get';
+        const params = {
+            category
+        };
+        dispatch(emitAPIRequest(endpoint, params, settingsReceived));
     }
 }
 
