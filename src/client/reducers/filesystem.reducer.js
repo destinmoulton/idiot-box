dad0c97d402f035688b9f3f80a2e52d6ed22833e
Destin Moulton <destin.moulton@gmail.com>
2017-07-05 15:57:14 -0600

    Removed dir list from filesystem actions/reducer.

--- src/client/reducers/filesystem.reducer.js
+++ src/client/reducers/filesystem.reducer.js
@@ -1,21 +1,14 @@
 import {
-    FS_DIR_RECEIVED,
+
 } from '../actions/actionTypes';
 
 const INITIAL_STATE = {
-    retrievalInProgress: false,
-    dirs: {}
+
 };
 
 export default function filesystemReducer(state = INITIAL_STATE, action){
     switch(action.type){
-        case FS_DIR_RECEIVED:
-            const newDirs = {...state.dirs};
-            newDirs[action.dir.requestID] = action.dir.list;
-            return {
-                ...state,
-                dirs: newDirs
-            }
+
         default:
             return state;
     }
