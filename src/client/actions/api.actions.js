3e3c39bcc824cfe54f31f0581e17268b44f93e2e
Destin Moulton <destin.moulton@gmail.com>
2017-07-05 15:59:50 -0600

    Added a shouldDispatch flag.

--- src/client/actions/api.actions.js
+++ src/client/actions/api.actions.js
@@ -13,7 +13,7 @@ let apiRequests = {};
 export function setupAPI(){
     return (dispatch)=>{
         dispatch(listenForAPIErrors());
-        dispatch(listenForAPIResponse());
+        dispatch(listenForAPIResponse(dispatch));
     }
 }
 
@@ -24,31 +24,37 @@ function listenForAPIErrors(){
         promise: (socket) => {
             return socket.on('api.error', (recd)=>{
                 console.error(recd.message);
-                console.error(recd.originalRequest);
+                console.error("Request contents: ", recd.request);
             });
         }
     }
 }
 
-function listenForAPIResponse(){
+function listenForAPIResponse(dispatch){
     return {
         type: 'socket',
         types: [IO_ON_SETUP, IO_ON_SUCCESS, IO_ON_FAILURE],
         promise: (socket) => {
             return socket.on('api.response', (recd)=>{
                 console.log("api.response received", recd);
-                dispatchAPICallback(recd);
+                if (apiRequests[recd.id].shouldDispatch) {
+                    dispatch(apiRequests[recd.id].dispatchCallback(recd.data, recd));
+                } else {
+                    apiRequests[recd.id].dispatchCallback(recd.data, recd);
+                }
+                delete apiRequests[recd.id];
             });
         }
     }
 }
 
-export function emitAPIRequest(endpoint, params, dispatchCallback){
+export function emitAPIRequest(endpoint, params, dispatchCallback, shouldDispatch = true){
     apiID++;
     apiRequests[apiID] = {
         endpoint,
         params,
-        dispatchCallback
+        dispatchCallback,
+        shouldDispatch
     };
     const request = {
         id: apiID,
@@ -62,11 +68,4 @@ export function emitAPIRequest(endpoint, params, dispatchCallback){
             return socket.emit('api.request', request);
         }
     }
-}
-
-function dispatchAPICallback(recd){
-    return (dispatch)=>{
-        dispatch(apiRequests[recd.id].dispatchCallback(recd.data, recd));
-        delete apiRequests[recd.id];
-    };
 }
