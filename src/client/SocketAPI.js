bee4c6c3226f8b2599d0b92b57445daf51be9d87
Destin Moulton <destin.moulton@gmail.com>
2017-07-05 12:07:28 -0600

    Added reconnect function (so a new socket is not made).

--- src/client/SocketAPI.js
+++ src/client/SocketAPI.js
@@ -8,8 +8,14 @@ export default class SocketAPI {
   socket;
 
   connect() {
-
     this.socket = io.connect(host, { path: socketPath });
+    return new Promise((resolve, reject) => {
+      this.socket.on('connect', () => resolve());
+      this.socket.on('connect_error', (error) => reject(error));
+    });
+  }
+
+  reconnect(){
     return new Promise((resolve, reject) => {
       this.socket.on('connect', () => resolve());
       this.socket.on('connect_error', (error) => reject(error));
