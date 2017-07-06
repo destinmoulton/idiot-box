88601899cf7f0a250b74a98f582f101961bc3d73
Destin Moulton <destin.moulton@gmail.com>
2017-07-06 13:44:24 -0600

    Added actions for saving settings.

--- src/client/actions/settings.actions.js
+++ src/client/actions/settings.actions.js
@@ -1,7 +1,9 @@
 import { emitAPIRequest } from './api.actions';
 
 import {
-    SETTING_LIST_RECEIVED
+    SETTING_LIST_RECEIVED,
+    SETTING_SAVE_START,
+    SETTING_SAVE_COMPLETE
 } from './actionTypes';
 
 export function getSettingsForCategory(category){
@@ -20,5 +22,44 @@ function settingsReceived(settings, recd){
         type: SETTINGS_LIST_RECEIVED,
         category: recd.request.params.category,
         settings
+    }
+}
+
+export function saveSetting(settingID, category, key, value){
+    return (dispatch)=>{
+        let endpoint = '';
+        let params = {};
+        if(settingID === 0){
+            endpoint = 'settings.editor.add';
+            params = {
+                category,
+                key,
+                value
+            };
+        } else {
+            endpoint = 'settings.editor.update';
+            params = {
+                id: settingID,
+                category,
+                key,
+                value
+            };
+        }
+        dispatch(settingSaveInProgress(settingID));
+        dispatch(emitAPIRequest(endpoint, params, settingSaveComplete));
+    }
+}
+
+function settingSaveInProgress(settingID){
+    return {
+        type:SETTING_SAVE_START,
+        settingID
+    }
+}
+
+function settingSaveComplete(status, recd){
+    return {
+        type:SETTING_SAVE_COMPLETE,
+        settingID
     }
 }
