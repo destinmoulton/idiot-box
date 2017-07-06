392e756cb4646c96c212b3c571eeab7140e7ad3d
Destin Moulton <destin.moulton@gmail.com>
2017-07-06 13:44:08 -0600

    Added save start/complete reducer sections.

--- src/client/reducers/settings.reducer.js
+++ src/client/reducers/settings.reducer.js
@@ -1,12 +1,15 @@
 import {
-    SETTING_LIST_RECEIVED
+    SETTING_LIST_RECEIVED,
+    SETTING_SAVE_START,
+    SETTING_SAVE_COMPLETE
 } from '../actions/actionTypes';
 
 
 const INITIAL_STATE = {
     settings:{
         directories: []
-    }
+    },
+    savesInProgress:[]
 }
 
 export default function settingsReducer(state = INITIAL_STATE, action){
@@ -17,6 +20,20 @@ export default function settingsReducer(state = INITIAL_STATE, action){
             return {
                 ...state,
                 settings
+            }
+        case SETTING_SAVE_START:
+            let savesInProgressStart = [...state.savesInProgress];
+            savesInProgressStart.push(action.settingID);
+            return {
+                ...state,
+                savesInProgress: savesInProgressStart
+            }
+        case SETTING_SAVE_COMPLETE:
+            let savesInProgressComplete = [...state.savesInProgress];
+            savesInProgressComplete.splice(savesInProgressComplete.indexOf(action.settingID), 1);
+            return {
+                ...state,
+                savesInProgress: savesInProgressComplete
             }
         default:
             return state;
