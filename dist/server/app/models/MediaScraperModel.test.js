1eae7e8c04303ed812871bfff084c226714ed44e
Destin Moulton <destin.moulton@gmail.com>
2017-07-07 16:22:11 -0600

    Added tests for getting the seasons and the episodes for a season.

--- dist/server/app/models/MediaScraperModel.test.js
+++ dist/server/app/models/MediaScraperModel.test.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _logger = require('../logger');
+
+var _logger2 = _interopRequireDefault(_logger);
+
 var _trakt = require('trakt.tv');
 
 var _trakt2 = _interopRequireDefault(_trakt);
@@ -26,6 +30,30 @@ describe("MediaScrapeModel", function () {
         return mediaScraper.searchMovies('tron').then(function (res) {
             expect(res[1].type).toBe('movie');
             expect(res[1].movie.title).toBe('TRON: Legacy');
+        });
+    });
+
+    test("finds tv shows", function () {
+        expect.assertions(2);
+        return mediaScraper.searchShows('days').then(function (res) {
+            expect(res[1].type).toBe('show');
+            expect(res[1].show.title).toBe('Day Break');
+        });
+    });
+
+    test("get a summary of a show's seasons", function () {
+        expect.assertions(1);
+        return mediaScraper.getShowSeasonsList(107460).then(function (res) {
+            expect(res[2].ids.trakt).toBe(137165);
+        });
+    });
+
+    test("get the episodes for a season", function () {
+        expect.assertions(3);
+        return mediaScraper.getEpisodesForSeason(107460, 1).then(function (res) {
+            expect(res.length).toBe(24);
+            expect(res[9].number).toBe(10);
+            expect(res[9].ids.trakt).toBe(2323304);
         });
     });
 });
--- src/server/models/MediaScraperModel.test.js
+++ src/server/models/MediaScraperModel.test.js
@@ -1,3 +1,4 @@
+import logger from '../logger';
 import Trakt from 'trakt.tv';
 import traktConfig from '../config/trakt.config';
 import MediaScraperModel from './MediaScraperModel';
@@ -15,6 +16,33 @@ describe("MediaScrapeModel", ()=>{
             .then((res)=>{
                 expect(res[1].type).toBe('movie');
                 expect(res[1].movie.title).toBe('TRON: Legacy');
+            });
+    });
+
+    test("finds tv shows", ()=>{
+        expect.assertions(2);
+        return mediaScraper.searchShows('days')
+            .then((res)=>{
+                expect(res[1].type).toBe('show');
+                expect(res[1].show.title).toBe('Day Break');
+            });
+    });
+
+    test("get a summary of a show's seasons", ()=>{
+        expect.assertions(1);
+        return mediaScraper.getShowSeasonsList(107460)
+            .then((res)=>{
+                expect(res[2].ids.trakt).toBe(137165);
+            })
+    });
+
+    test("get the episodes for a season", ()=>{
+        expect.assertions(3);
+        return mediaScraper.getEpisodesForSeason(107460, 1)
+            .then((res)=>{
+                expect(res.length).toBe(24);
+                expect(res[9].number).toBe(10);
+                expect(res[9].ids.trakt).toBe(2323304);
             });
     });
 });
