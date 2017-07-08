044a881f34f29fa584559273145500ef32f179b2
Destin Moulton <destin.moulton@gmail.com>
2017-07-08 11:05:19 -0600

    Added extended: 'full' fields to the tests.

--- src/server/models/MediaScraperModel.test.js
+++ src/server/models/MediaScraperModel.test.js
@@ -20,29 +20,32 @@ describe("MediaScrapeModel", ()=>{
     });
 
     test("finds tv shows", ()=>{
-        expect.assertions(2);
+        expect.assertions(3);
         return mediaScraper.searchShows('days')
             .then((res)=>{
                 expect(res[1].type).toBe('show');
                 expect(res[1].show.title).toBe('Day Break');
+                expect(res[1].show.status).toBe('ended');
             });
     });
 
-    test("get a summary of a show's seasons", ()=>{
-        expect.assertions(1);
+    test("get a list of a show's seasons", ()=>{
+        expect.assertions(2);
         return mediaScraper.getShowSeasonsList(107460)
             .then((res)=>{
                 expect(res[2].ids.trakt).toBe(137165);
+                expect(res[2].episode_count).toBe(1);
             })
     });
 
     test("get the episodes for a season", ()=>{
-        expect.assertions(3);
+        expect.assertions(4);
         return mediaScraper.getEpisodesForSeason(107460, 1)
             .then((res)=>{
                 expect(res.length).toBe(24);
                 expect(res[9].number).toBe(10);
                 expect(res[9].ids.trakt).toBe(2323304);
+                expect(res[9].first_aired).toBe("2016-09-03T17:58:00.000Z");
             });
     });
 });
