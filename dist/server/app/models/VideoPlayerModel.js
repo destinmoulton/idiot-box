2a74577c3097d92900803ecd27fae74e3a7a751f
Destin Moulton <destin.moulton@gmail.com>
2017-07-13 15:01:36 -0600

    Created Video Player Model

---
+++ dist/server/app/models/VideoPlayerModel.js
@@ -0,0 +1,108 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var VideoPlayerModel = function () {
+    function VideoPlayerModel(omxPlayer, audioOutput) {
+        _classCallCheck(this, VideoPlayerModel);
+
+        this.player = {};
+        this.audioOutput = "";
+
+        this.player = omxPlayer;
+
+        this.audioOutput = audioOutput;
+    }
+
+    _createClass(VideoPlayerModel, [{
+        key: "start",
+        value: function start(filename) {
+            var _this = this;
+
+            return new Promise(function (resolve, reject) {
+                _this.player.newSource(filename, _this.audioOutput);
+                resolve(true);
+            });
+        }
+    }, {
+        key: "play",
+        value: function play() {
+            var _this2 = this;
+
+            return new Promise(function (resolve, reject) {
+                _this2.player.play();
+                resolve(true);
+            });
+        }
+    }, {
+        key: "pause",
+        value: function pause() {
+            var _this3 = this;
+
+            return new Promise(function (resolve, reject) {
+                _this3.player.pause();
+                resolve(true);
+            });
+        }
+    }, {
+        key: "fastFwd",
+        value: function fastFwd() {
+            var _this4 = this;
+
+            return new Promise(function (resolve, reject) {
+                _this4.player.fastFwd();
+                resolve(true);
+            });
+        }
+    }, {
+        key: "rewind",
+        value: function rewind() {
+            var _this5 = this;
+
+            return new Promise(function (resolve, reject) {
+                _this5.player.rewind();
+                resolve(true);
+            });
+        }
+    }, {
+        key: "fwd30",
+        value: function fwd30() {
+            var _this6 = this;
+
+            return new Promise(function (resolve, reject) {
+                _this6.player.fwd30();
+                resolve(true);
+            });
+        }
+    }, {
+        key: "back30",
+        value: function back30() {
+            var _this7 = this;
+
+            return new Promise(function (resolve, reject) {
+                _this7.player.back30();
+                resolve(true);
+            });
+        }
+    }, {
+        key: "toggleSubtitles",
+        value: function toggleSubtitles() {
+            var _this8 = this;
+
+            return new Promise(function (resolve, reject) {
+                _this8.player.subtitles();
+                resolve(true);
+            });
+        }
+    }]);
+
+    return VideoPlayerModel;
+}();
+
+exports.default = VideoPlayerModel;
