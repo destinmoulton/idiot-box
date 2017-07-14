c87e3e75b177f0bec6802dc207098ddf31a5c0dd
Destin Moulton <destin.moulton@gmail.com>
2017-07-14 11:59:24 -0600

    Fixed winston prettyPrint issue.

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,39 +1,39 @@
 const winston = require('winston');
 
-const customColors = {
-  trace: 'white',
-  debug: 'blue',
-  info: 'green',
-  warn: 'yellow',
-  crit: 'red',
-  error: 'red'
-};
+winston.setLevels({
+    trace: 9,
+    input: 8,
+    verbose: 7,
+    prompt: 6,
+    debug: 5,
+    info: 4,
+    data: 3,
+    help: 2,
+    warn: 1,
+    error: 0
+});
 
-let config = {
-  colors: customColors,
+winston.addColors({
+    trace: 'magenta',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    debug: 'blue',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    error: 'red'
+});
 
-  levels: {
-    trace: 0,
-    debug: 1,
-    info: 2,
-    warn: 3,
-    crit: 4,
-    error: 5
-  },
-  transports: [
-  ]
-};
-
-config.transports.push(
-  new (winston.transports.Console)({
-    name: 'consoleLogger',
-    level: 'error',
+winston.remove(winston.transports.Console)
+winston.add(winston.transports.Console, {
+    level: 'trace',
+    prettyPrint: true,
     colorize: true,
-    timestamp: true
-  })
-);
+    silent: false,
+    timestamp: false
+});
 
-const logger = new (winston.Logger)(config);
-winston.addColors(customColors);
-
+const logger = winston;
 export default logger;
