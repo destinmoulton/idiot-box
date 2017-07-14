a3357f3a3d3a716fb7e0cb0cd11cf7c37414b8ac
Destin Moulton <destin.moulton@gmail.com>
2017-07-14 12:02:49 -0600

    Added video info actions.

--- src/client/actions/videoplayer.actions.js
+++ src/client/actions/videoplayer.actions.js
@@ -1,22 +1,37 @@
 import { emitAPIRequest } from './api.actions';
 
 import {
-    VP_CHANGE_STATE
+    VP_CHANGE_STATE,
+    VP_RECEIVED_INFO
 } from './actionTypes';
 
-export function isVideoRunning(){
+export function isPlayerRunning(){
     return (dispatch)=>{
         setTimeout(()=>{
             const endpoint = 'videoplayer.cmd.isRunning';
-            dispatch(emitAPIRequest(endpoint, {}, videoRunningChangeState));
-            isVideoRunning();
+            dispatch(emitAPIRequest(endpoint, {}, playerRunningChangeState));
+            isPlayerRunning();
         }, 200);
     }
 }
 
-function videoRunningChangeState(playerState){
+function playerRunningChangeState(playerState){
     return {
         type: VP_CHANGE_STATE,
         playerState
+    }
+}
+
+export function getVideoInfo(){
+    return (dispatch)=>{
+        const endpoint = 'videoplayer.cmd.info';
+        dispatch(emitAPIRequest(endpoint, {}, videoReceivedInfo));
+    }
+}
+
+function videoReceivedInfo(videoInfo){
+    return {
+        type: VP_RECEIVED_INFO,
+        videoInfo
     }
 }
