21065ee8c7ace769503c2a60d76d9f2f57a377b3
Destin Moulton <destin.moulton@gmail.com>
2017-07-15 15:44:19 -0600

    Added methods for trashing files.

--- src/client/Components/Filesystem/TrashModal.js
+++ src/client/Components/Filesystem/TrashModal.js
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { Modal } from 'antd';
+import { Button, Modal } from 'antd';
+import { emitAPIRequest } from '../../actions/api.actions';
 
 class TrashModal extends Component {
     static propTypes = {
@@ -16,14 +18,54 @@ class TrashModal extends Component {
         
     };
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isTrashing: false
+        };
+    }
+
     _handlePressOk(){
+        this._trashFiles();
+    }
+
+    _trashFiles(){
+        const { 
+            currentPath,
+            emitAPIRequest,
+            itemsToTrash } = this.props;
+
+        const options = {
+            source_path: currentPath,
+            filenames: itemsToTrash
+        };
+
+        emitAPIRequest("filesystem.trash.execute", options, this._trashComplete.bind(this), false);
+
+        this.setState({
+            isTrashing: true
+        });
+    }
+
+    _trashComplete(recd){
         const { onTrashComplete } = this.props;
+
+        this.setState({
+            isTrashing: false
+        });
 
         onTrashComplete();
     }
 
     render() {
-        const { currentPath, isVisible, itemsToTrash, onCancel } = this.props;
+        const { 
+            currentPath,
+            isVisible,
+            itemsToTrash,
+            onCancel } = this.props;
+
+        const { isTrashing } = this.state;
 
         let list = [];
         itemsToTrash.forEach((item)=>{
@@ -37,8 +79,12 @@ class TrashModal extends Component {
                     visible={isVisible}
                     onCancel={onCancel}
                     onOk={this._handlePressOk.bind(this)}
-                    okText="Confirm Delete"
-                    cancelText="Cancel"
+                    footer={[
+                        <Button key="cancel" size="large" onClick={onCancel}>Cancel</Button>,
+                        <Button key="submit" type="primary" size="large" loading={isTrashing} onClick={this._handlePressOk.bind(this)}>
+                        Confirm Delete
+                        </Button>,
+                    ]}
                 >
                     <div className="ib-trashmodal-list-box" >
                         <h4>{currentPath}</h4>
@@ -50,4 +96,14 @@ class TrashModal extends Component {
     }
 }
 
-export default TrashModal;
+const mapStateToProps = (state) => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        emitAPIRequest: (endpoint, params, callback, shouldDispatch)=>dispatch(emitAPIRequest(endpoint, params, callback, shouldDispatch))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrashModal);
