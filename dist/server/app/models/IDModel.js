51fb9703a1b28dee19928ffe6c22694737515f9b
Destin Moulton <destin.moulton@gmail.com>
2017-07-20 15:25:27 -0600

    Added method to Add a show and id an episode.

--- dist/server/app/models/IDModel.js
+++ dist/server/app/models/IDModel.js
@@ -13,10 +13,14 @@ var IDModel = function () {
         _classCallCheck(this, IDModel);
 
         this._filesModel = models.filesModel;
+        this._fileToEpisodeModel = models.fileToEpisodeModel;
         this._fileToMovieModel = models.fileToMovieModel;
         this._mediaScraperModel = models.mediaScraperModel;
         this._moviesModel = models.moviesModel;
         this._settingsModel = models.settingsModel;
+        this._showsModel = models.showsModel;
+        this._showSeasonsModel = models.showSeasonsModel;
+        this._showSeasonEpisodesModel = models.showSeasonEpisodesModel;
     }
 
     _createClass(IDModel, [{
@@ -24,7 +28,7 @@ var IDModel = function () {
         value: function idMovie(movieInfo, fileInfo, imageInfo) {
             var _this = this;
 
-            var imageFilename = movieInfo.title + "." + movieInfo.year;
+            var imageFilename = this._buildThumbFilename(movieInfo);
             return this._mediaScraperModel.downloadThumbnail("Movie", imageInfo.url, imageFilename).then(function (imageFilename) {
                 return _this._moviesModel.addMovie(movieInfo, imageFilename);
             }).then(function (movieRow) {
@@ -34,24 +38,61 @@ var IDModel = function () {
             });
         }
     }, {
+        key: "idEpisode",
+        value: function idEpisode(epInfo, fileInfo) {
+            var _this2 = this;
+
+            return this._filesModel.addFile(fileInfo.setting_id, fileInfo.subpath, fileInfo.filename, "show").then(function (fileRow) {
+                return _this2._fileToEpisodeModel.add(fileRow.id, epInfo.show_id, epInfo.season_id, epInfo.episode_id);
+            });
+        }
+    }, {
+        key: "addShow",
+        value: function addShow(showInfo, imageInfo) {
+            var _this3 = this;
+
+            var imageFilename = this._buildThumbFilename(showInfo);
+            return this._mediaScraperModel.downloadThumbnail("Show", imageInfo.url, imageFilename).then(function (imageFilename) {
+                return _this3._showsModel.addShow(showInfo, imageFilename);
+            }).then(function (show) {
+                return _this3._mediaScraperModel.getShowSeasonsList(show.trakt_id).then(function (seasons) {
+                    return _this3._showSeasonsModel.addArrayOfSeasons(seasons, show.id);
+                }).then(function (addedSeasons) {
+                    var promisesToRun = [];
+                    addedSeasons.forEach(function (season) {
+                        var prom = _this3._mediaScraperModel.getEpisodesForSeason(show.trakt_id, season.season_number).then(function (episodesArr) {
+                            return _this3._showSeasonEpisodesModel.addArrEpisodes(show.id, season.id, episodesArr);
+                        });
+                        promisesToRun.push(prom);
+                    });
+                    return Promise.all(promisesToRun);
+                });
+            });
+        }
+    }, {
+        key: "_buildThumbFilename",
+        value: function _buildThumbFilename(mediaInfo) {
+            return mediaInfo.title + "." + mediaInfo.year;
+        }
+    }, {
         key: "findID",
         value: function findID(fileInfo) {
-            var _this2 = this;
+            var _this4 = this;
 
             var subpath = fileInfo.fullPath.slice(fileInfo.basePath.length + 1);
             return this._settingsModel.getSingleByCatAndVal("directories", fileInfo.basePath).then(function (setting) {
                 if (!'id' in setting) {
                     return {};
                 }
-                return _this2._filesModel.getSingleByDirectoryAndFilename(setting.id, subpath, fileInfo.filename);
+                return _this4._filesModel.getSingleByDirectoryAndFilename(setting.id, subpath, fileInfo.filename);
             }).then(function (file) {
                 if (!'id' in file) {
                     return {};
                 }
 
                 if (file.mediatype === "movie") {
-                    return _this2._fileToMovieModel.getSingleForFile(file.id).then(function (fileToMovie) {
-                        return _this2._moviesModel.getSingle(fileToMovie.movie_id);
+                    return _this4._fileToMovieModel.getSingleForFile(file.id).then(function (fileToMovie) {
+                        return _this4._moviesModel.getSingle(fileToMovie.movie_id);
                     });
                 } else {
                     return {};
