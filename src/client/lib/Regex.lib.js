7c62339d8f624296698861319c9790069a9250c6
Destin Moulton <destin.moulton@gmail.com>
2017-07-27 14:39:52 -0600

    Added method to parse the S##E## from an episode's filename.

--- src/client/lib/Regex.lib.js
+++ src/client/lib/Regex.lib.js
@@ -1,6 +1,6 @@
 
 class Regex {
-    getShowName(showTitle){
+    sanitizeShowTitle(showTitle){
         // Replace current periods
         let newTitle = showTitle.replace(/\./g, "");
 
@@ -9,6 +9,23 @@ class Regex {
 
         // Replace everything else with blank
         return newTitle.replace(/[^\.a-zA-Z0-9]/g, "");
+    }
+
+    /**
+     * Get the S##E## from an episode filename.
+     * 
+     * ie. Desperate.Housewives.S01E04.mp4
+     *     will return S01E04 
+     *
+     * @param string episodeFilename 
+     */
+    parseSeasonEpisodeDoublet(episodeFilename){
+        let possibleIndex = episodeFilename.search(/S\d\dE\d\d/);
+
+        if(possibleIndex > -1){
+            return episodeFilename.substr(possibleIndex, 6);
+        }
+        return "";
     }
 }
 
