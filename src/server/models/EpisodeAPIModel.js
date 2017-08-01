e1fb983b84dd85a7df85c075057a2776f63a911d
Destin Moulton <destin.moulton@gmail.com>
2017-08-01 15:02:38 -0600

    Created Episode API Model for collating the episode information.

---
+++ src/server/models/EpisodeAPIModel.js
@@ -0,0 +1,40 @@
+import logger from '../logger';
+
+export default class EpisodeAPIModel {
+
+    constructor(models){
+        this._showSeasonEpisodesModel = models.showSeasonEpisodesModel;
+        this._filesModel = models.filesModel;
+        this._fileToEpisodeModel = models.fileToEpisodeModel;
+    }
+
+    getAllEpisodesWithFileInfo(showID, seasonID){
+        return this._showSeasonEpisodesModel.getEpisodesForSeason(showID, seasonID)
+                .then((episodes)=>{
+                    let promisesToRun = [];
+                    
+                    episodes.forEach((ep)=>{
+                        let data = Object.assign({}, ep);
+                        data['file_info'] = {};
+                        
+                        const cmd = this._fileToEpisodeModel.getSingleForEpisode(ep.id)
+                                        .then((fileEp)=>{
+                                            if(!fileEp.hasOwnProperty('file_id')){
+                                                return Promise.resolve(data);
+                                            }
+                                            return this._filesModel.getSingle(fileEp.file_id)
+                                                    .then((file)=>{
+                                                        if(!file.hasOwnProperty('id')){
+                                                            return Promise.resolve(data);
+                                                        }
+                                                        data.file_info = file;
+                                                        return Promise.resolve(data);
+                                                    });
+                                        });
+                        promisesToRun.push(cmd);
+                    });
+
+                    return Promise.all(promisesToRun);
+                });
+    }
+}
