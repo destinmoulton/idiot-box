a1bd339214734379fd89c6dc6c94f331040c38c3
Destin Moulton <destin.moulton@gmail.com>
2017-08-09 15:55:03 -0600

    Added movie delete endpoint.

--- dist/server/app/socket.io/apiendpoints/movies.api.js
+++ dist/server/app/socket.io/apiendpoints/movies.api.js
@@ -12,9 +12,21 @@ var _logger = require('../../logger');
 
 var _logger2 = _interopRequireDefault(_logger);
 
+var _FilesModel = require('../../models/db/FilesModel');
+
+var _FilesModel2 = _interopRequireDefault(_FilesModel);
+
+var _FileToMovieModel = require('../../models/db/FileToMovieModel');
+
+var _FileToMovieModel2 = _interopRequireDefault(_FileToMovieModel);
+
 var _GenresModel = require('../../models/db/GenresModel');
 
 var _GenresModel2 = _interopRequireDefault(_GenresModel);
+
+var _MovieAPI = require('../../models/MovieAPI');
+
+var _MovieAPI2 = _interopRequireDefault(_MovieAPI);
 
 var _MovieToGenreModel = require('../../models/db/MovieToGenreModel');
 
@@ -26,11 +38,30 @@ var _MoviesModel2 = _interopRequireDefault(_MoviesModel);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var filesModel = new _FilesModel2.default(_IBDB2.default);
+var fileToMovieModel = new _FileToMovieModel2.default(_IBDB2.default);
 var genresModel = new _GenresModel2.default(_IBDB2.default);
 var movieToGenreModel = new _MovieToGenreModel2.default(_IBDB2.default, genresModel);
 var moviesModel = new _MoviesModel2.default(_IBDB2.default, movieToGenreModel);
 
+var movieAPIModels = {
+    filesModel: filesModel,
+    fileToMovieModel: fileToMovieModel,
+    movieToGenreModel: movieToGenreModel,
+    moviesModel: moviesModel
+};
+
+var movieAPI = new _MovieAPI2.default(movieAPIModels);
+
 var movies = {
+    movie: {
+        delete: {
+            params: ['movie_id'],
+            func: function func(movieID) {
+                return movieAPI.deleteSingle(movieID);
+            }
+        }
+    },
     movies: {
         get_all: {
             params: [],
--- src/server/socket.io/apiendpoints/movies.api.js
+++ src/server/socket.io/apiendpoints/movies.api.js
@@ -1,15 +1,35 @@
 import ibdb from '../../db/IBDB';
 import logger from '../../logger';
 
+import FilesModel from '../../models/db/FilesModel';
+import FileToMovieModel from '../../models/db/FileToMovieModel';
 import GenresModel from '../../models/db/GenresModel';
+import MovieAPI from '../../models/MovieAPI';
 import MovieToGenreModel from '../../models/db/MovieToGenreModel';
 import MoviesModel from '../../models/db/MoviesModel';
 
+const filesModel = new FilesModel(ibdb);
+const fileToMovieModel = new FileToMovieModel(ibdb);
 const genresModel = new GenresModel(ibdb);
 const movieToGenreModel = new MovieToGenreModel(ibdb, genresModel);
 const moviesModel = new MoviesModel(ibdb, movieToGenreModel);
 
+const movieAPIModels = {
+    filesModel,
+    fileToMovieModel,
+    movieToGenreModel,
+    moviesModel
+};
+
+const movieAPI = new MovieAPI(movieAPIModels);
+
 const movies = {
+    movie: {
+        delete: {
+            params: ['movie_id'],
+            func: (movieID)=> movieAPI.deleteSingle(movieID)
+        }
+    },
     movies: {
         get_all: {
             params: [],
