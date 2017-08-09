e4f7a61eebf2c6aa967ec43acde835f9ecceed57
Destin Moulton <destin.moulton@gmail.com>
2017-08-09 15:55:53 -0600

    Added delete movie feature.

--- src/client/Components/Movies/MovieInfoModal.js
+++ src/client/Components/Movies/MovieInfoModal.js
@@ -1,14 +1,39 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { Button, Col, Modal, Row } from 'antd';
 
+import { emitAPIRequest } from '../../actions/api.actions';
 class MovieInfoModal extends Component {
     static propTypes = {
         isVisible: PropTypes.bool.isRequired,
         movie: PropTypes.object.isRequired,
         onClose: PropTypes.func.isRequired 
     };
+
+    _handlePressDelete(){
+        const { movie } = this.props;
+
+        if(confirm(`Really delete ${movie.title}`)){
+            this._deleteMovie();
+        }
+    }
+
+    _deleteMovie(){
+        const { emitAPIRequest, movie } = this.props;
+
+        const params = {
+            movie_id: movie.id
+        };
+
+        emitAPIRequest("movies.movie.delete", params, this._movieDeleted.bind(this), false);
+    }
+
+    _movieDeleted(){
+        const { onClose } = this.props;
+        onClose(true);
+    }
 
     render() {
         const {
@@ -38,6 +63,7 @@ class MovieInfoModal extends Component {
                         <h3 dangerouslySetInnerHTML={{__html: movie.title}}></h3>
                         <h4>{movie.year}</h4>
                         <h4><a href={"http://imdb.com/title/" + movie.imdb_id} target="_blank">IMDB</a></h4>
+                        <Button onClick={this._handlePressDelete.bind(this)}>Delete</Button>
                         <p>{movie.overview}</p>
                     </Col>
                 </Row>
@@ -46,4 +72,14 @@ class MovieInfoModal extends Component {
     }
 }
 
-export default MovieInfoModal;
+const mapStateToProps = (state)=>{
+    return {};
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        emitAPIRequest: (endpoint, params, callback, shouldDispatch)=>dispatch(emitAPIRequest(endpoint, params, callback, shouldDispatch))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieInfoModal);
