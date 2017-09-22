986cbd113326e394420fedb365dfe1c8fd240097
Destin Moulton <destin.moulton@gmail.com>
2017-09-22 11:42:56 -0600

    Added second data set; renamed data method to collate expectations.

--- dist/server/app/models/db/ShowsModel.test.js
+++ dist/server/app/models/db/ShowsModel.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
 
 var _moment = require('moment');
 
@@ -25,7 +27,7 @@ describe("ShowsModel", function () {
         };
 
         var migConfig = {
-            migrationsPath: path.resolve(__dirname, '../../../migrations')
+            migrationsPath: _path2.default.resolve(__dirname, '../../../migrations')
         };
 
         return _IBDB2.default.connect(dbConfig).then(function () {
@@ -40,68 +42,95 @@ describe("ShowsModel", function () {
     });
 
     it("adds a show", function () {
-        var _getTestData = getTestData(),
-            _getTestData2 = _slicedToArray(_getTestData, 3),
-            API_DATA = _getTestData2[0],
-            EXPECTED_DATA = _getTestData2[1],
-            IMAGE_FILENAME = _getTestData2[2];
-
-        return showsModel.addShow(API_DATA);
+        expect.assertions(1);
+        var IMAGE_FILENAME = "FILANEME_ONE.png";
+        var EXPECTED_DATA = collateExpectation(DATASET_ONE, IMAGE_FILENAME);
+        return showsModel.addShow(DATASET_ONE, IMAGE_FILENAME).then(function (res) {
+            expect(res).toMatchObject(EXPECTED_DATA);
+        });
     });
 });
 
-function getTestData() {
-    // Data taken directly from an API call
-    var API_DATA = {
-        title: 'Day Break',
-        year: 2006,
-        ids: {
-            trakt: 4594,
-            slug: 'day-break',
-            tvdb: 79509,
-            imdb: 'tt0801425',
-            tmdb: 4618,
-            tvrage: 8152
-        },
-        overview: 'Today Detective Brett Hopper will be accused of shooting state attorney Alberto Garza. He will offer his rock solid alibi. He will realize he\'s been framed. And he will run. Then, he will wake up and start the day over again.',
-        first_aired: '2006-11-16T02:00:00.000Z',
-        airs: { day: 'Wednesday', time: '21:00', timezone: 'America/New_York' },
-        runtime: 42,
-        certification: 'TV-14',
-        network: 'ABC (US)',
-        country: 'us',
-        trailer: null,
-        homepage: null,
-        status: 'ended',
-        rating: 7.76882,
-        votes: 372,
-        updated_at: '2017-07-06T09:28:20.000Z',
-        language: 'en',
-        available_translations: ['bg', 'bs', 'el', 'en', 'es', 'fr', 'ru', 'tr'],
-        genres: ['drama', 'action', 'adventure', 'fantasy', 'science-fiction'],
-        aired_episodes: 13
-    };
-
-    var IMAGE_FILENAME = "TEST_FILENAME.jpg";
-
+function collateExpectation(apiData, imageFilename) {
     var EXPECTED_DATA = {
-        title: API_DATA.title,
-        year: API_DATA.year,
-        overview: API_DATA.overview,
-        first_aired: (0, _moment2.default)(API_DATA.first_aired).format('X'),
-        runtime: API_DATA.runtime,
-        network: API_DATA.network,
-        status: API_DATA.status,
-        rating: API_DATA.rating,
-        updated_at: (0, _moment2.default)(API_DATA.updated_at).format('X'),
-        slug: API_DATA.ids.slug,
-        trakt_id: API_DATA.ids.trakt,
-        tvdb_id: API_DATA.ids.tvdb,
-        imdb_id: API_DATA.ids.imdb,
-        tmdb_id: API_DATA.ids.tmdb,
-        tvrage_id: API_DATA.ids.tvrage,
-        image_filename: IMAGE_FILENAME
+        title: apiData.title,
+        year: apiData.year,
+        overview: apiData.overview,
+        first_aired: parseInt((0, _moment2.default)(apiData.first_aired).format('X')),
+        runtime: apiData.runtime,
+        network: apiData.network,
+        status: apiData.status,
+        rating: apiData.rating,
+        updated_at: parseInt((0, _moment2.default)(apiData.updated_at).format('X')),
+        slug: apiData.ids.slug,
+        trakt_id: apiData.ids.trakt,
+        tvdb_id: apiData.ids.tvdb,
+        imdb_id: apiData.ids.imdb,
+        tmdb_id: apiData.ids.tmdb,
+        tvrage_id: apiData.ids.tvrage,
+        image_filename: imageFilename
     };
 
-    return [API_DATA, EXPECTED_DATA, IMAGE_FILENAME];
+    return EXPECTED_DATA;
 }
+
+// Data taken directly from an API call
+var DATASET_ONE = {
+    title: 'Day Break',
+    year: 2006,
+    ids: {
+        trakt: 4594,
+        slug: 'day-break',
+        tvdb: 79509,
+        imdb: 'tt0801425',
+        tmdb: 4618,
+        tvrage: 8152
+    },
+    overview: 'Today Detective Brett Hopper will be accused of shooting state attorney Alberto Garza. He will offer his rock solid alibi. He will realize he\'s been framed. And he will run. Then, he will wake up and start the day over again.',
+    first_aired: '2006-11-16T02:00:00.000Z',
+    airs: { day: 'Wednesday', time: '21:00', timezone: 'America/New_York' },
+    runtime: 42,
+    certification: 'TV-14',
+    network: 'ABC (US)',
+    country: 'us',
+    trailer: null,
+    homepage: null,
+    status: 'ended',
+    rating: 7.76882,
+    votes: 372,
+    updated_at: '2017-07-06T09:28:20.000Z',
+    language: 'en',
+    available_translations: ['bg', 'bs', 'el', 'en', 'es', 'fr', 'ru', 'tr'],
+    genres: ['drama', 'action', 'adventure', 'fantasy', 'science-fiction'],
+    aired_episodes: 13
+};
+
+var DATASET_TWO = {
+    title: 'Happy Days',
+    year: 1974,
+    ids: {
+        trakt: 3822,
+        slug: 'happy-days',
+        tvdb: 74475,
+        imdb: 'tt0070992',
+        tmdb: 3845,
+        tvrage: 3785
+    },
+    overview: 'A nostalgic comedy set in 1950s Milwaukee centered on the squeaky-clean Cunningham family and their relationship with Fonzie, a motorcycle-riding Casanova who became a pop-culture phenomenon during the show\'s heyday in the 1970s.',
+    first_aired: '1974-01-15T00:00:00.000Z',
+    airs: { day: 'Tuesday', time: '20:00', timezone: 'America/New_York' },
+    runtime: 25,
+    certification: 'TV-PG',
+    network: 'ABC (US)',
+    country: 'us',
+    trailer: null,
+    homepage: null,
+    status: 'ended',
+    rating: 7.54359,
+    votes: 195,
+    updated_at: '2017-09-19T11:00:28.000Z',
+    language: 'en',
+    available_translations: ['en', 'fr', 'it', 'ru'],
+    genres: ['comedy'],
+    aired_episodes: 255
+};
