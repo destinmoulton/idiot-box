adc285bf428cefdfd166db27e2078a87e2aa1f6e
Destin Moulton <destin.moulton@gmail.com>
2017-09-22 10:48:56 -0600

    Added before/after each methods and showsModel initialization.

--- dist/server/app/models/db/ShowsModel.test.js
+++ dist/server/app/models/db/ShowsModel.test.js
@@ -1,3 +1,35 @@
-"use strict";
+'use strict';
 
-describe("ShowsModel", function () {});
+var _IBDB = require('../../db/IBDB');
+
+var _IBDB2 = _interopRequireDefault(_IBDB);
+
+var _ShowsModel = require('./ShowsModel');
+
+var _ShowsModel2 = _interopRequireDefault(_ShowsModel);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+describe("ShowsModel", function () {
+    var showsModel = {};
+
+    beforeEach(function () {
+        var dbConfig = {
+            inMemory: true
+        };
+
+        var migConfig = {
+            migrationsPath: path.resolve(__dirname, '../../../migrations')
+        };
+
+        return _IBDB2.default.connect(dbConfig).then(function () {
+            return _IBDB2.default._db.migrate(migConfig);
+        }).then(function () {
+            showsModel = new _ShowsModel2.default(_IBDB2.default);
+        });
+    });
+
+    afterEach(function () {
+        _IBDB2.default.close();
+    });
+});
--- src/server/models/db/ShowsModel.test.js
+++ src/server/models/db/ShowsModel.test.js
@@ -1,5 +1,30 @@
 
+import ibdb from '../../db/IBDB';
+
+import ShowsModel from './ShowsModel';
 
 describe("ShowsModel", ()=>{
+    let showsModel = {};
     
+    beforeEach(() => {
+        const dbConfig = {
+            inMemory: true
+        };
+
+        const migConfig = {
+            migrationsPath: path.resolve(__dirname, '../../../migrations')
+        };
+
+        return ibdb.connect(dbConfig)
+            .then(() => {
+                return ibdb._db.migrate(migConfig);
+            })
+            .then(()=>{
+                showsModel = new ShowsModel(ibdb);
+            });
+    });
+
+    afterEach(()=>{
+        ibdb.close();
+    });
 });
