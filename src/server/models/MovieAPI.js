bba8cd42e476cb4ffa7ee5e67417bb82f0e668bb
Destin Moulton <destin.moulton@gmail.com>
2018-02-07 09:44:54 -0700

    Changed promise resolution to allow movies to be deleted even if the movie has no file association.

--- src/server/models/MovieAPI.js
+++ src/server/models/MovieAPI.js
@@ -1,96 +1,102 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-import thumbConfig from '../config/thumbnails.config';
+import thumbConfig from "../config/thumbnails.config";
 
 class MovieAPI {
-    constructor(models){
+    constructor(models) {
         this._filesModel = models.filesModel;
         this._fileToMovieModel = models.fileToMovieModel;
         this._moviesModel = models.moviesModel;
         this._movieToGenreModel = models.movieToGenreModel;
     }
 
-    getAllMoviesWithFileInfo(){
-        return this._moviesModel.getAll()
-                .then((movies)=>{
-                    let promisesToRun = [];
-                    
-                    movies.forEach((movie)=>{
-                        let data = Object.assign({}, movie);
-                        data['file_info'] = {};
-                        
-                        const cmd = this._fileToMovieModel.getSingleForMovie(movie.id)
-                                        .then((fileMovie)=>{
-                                            if(!fileMovie.hasOwnProperty('file_id')){
-                                                return Promise.resolve(data);
-                                            }
-                                            return this._filesModel.getSingle(fileMovie.file_id)
-                                                    .then((file)=>{
-                                                        if(!file.hasOwnProperty('id')){
-                                                            return Promise.resolve(data);
-                                                        }
-                                                        data.file_info = file;
-                                                        return Promise.resolve(data);
-                                                    });
-                                        });
-                        promisesToRun.push(cmd);
-                    });
+    getAllMoviesWithFileInfo() {
+        return this._moviesModel.getAll().then(movies => {
+            let promisesToRun = [];
 
-                    return Promise.all(promisesToRun);
-                });
-    }
+            movies.forEach(movie => {
+                let data = Object.assign({}, movie);
+                data["file_info"] = {};
 
-    deleteSingle(movieID){
-        return this._moviesModel.getSingle(movieID)
-                .then((movie)=>{
-                    if(!movie.hasOwnProperty('id')){
-                        return Promise.reject("MovieAPI :: deleteSingle() :: Unable to find movie ${movieID}");
-                    }
-                    return this._removeMovieThumbnail(movie);
-                })
-                .then(()=>{
-                    return this._movieToGenreModel.deleteForMovie(movieID);
-                })
-                .then(()=>{
-                    return this._removeFileAssociationForMovie(movieID);
-                })
-                .then(()=>{
-                    return this._moviesModel.deleteSingle(movieID);
-                })
-                
-    }
-
-    _removeShowThumbnail(showID){
-        return this._showsModel.getSingle(showID)
-                .then((show)=>{
-                    const fullPath = path.join(thumbConfig.shows, show.image_filename);
-                    if(!fs.existsSync(fullPath)){
-                        return Promise.resolve(true);
-                    }
-                    return Promise.resolve(fs.unlinkSync(fullPath));
-                })
-
-    }
-
-    _removeFileAssociationForMovie(movieID){
-        return this._fileToMovieModel.getSingleForMovie(movieID)
-                .then((fileToMovie)=>{
-                    if(!fileToMovie.hasOwnProperty('file_id')){
-                        return Promise.reject("MovieAPI :: _removeFileAssociationForMovie :: Unable to find file to movie association");
-                    }
-
-                    return this._filesModel.deleteSingle(fileToMovie.file_id)
-                            .then(()=>{
-                                return this._fileToMovieModel.deleteSingle(fileToMovie.file_id, movieID);
+                const cmd = this._fileToMovieModel
+                    .getSingleForMovie(movie.id)
+                    .then(fileMovie => {
+                        if (!fileMovie.hasOwnProperty("file_id")) {
+                            return Promise.resolve(data);
+                        }
+                        return this._filesModel
+                            .getSingle(fileMovie.file_id)
+                            .then(file => {
+                                if (!file.hasOwnProperty("id")) {
+                                    return Promise.resolve(data);
+                                }
+                                data.file_info = file;
+                                return Promise.resolve(data);
                             });
-                });
-                
+                    });
+                promisesToRun.push(cmd);
+            });
+
+            return Promise.all(promisesToRun);
+        });
     }
 
-    _removeMovieThumbnail(movie){
+    deleteSingle(movieID) {
+        return this._moviesModel
+            .getSingle(movieID)
+            .then(movie => {
+                if (!movie.hasOwnProperty("id")) {
+                    return Promise.reject(
+                        "MovieAPI :: deleteSingle() :: Unable to find movie ${movieID}"
+                    );
+                }
+                return this._removeMovieThumbnail(movie);
+            })
+            .then(() => {
+                return this._movieToGenreModel.deleteForMovie(movieID);
+            })
+            .then(() => {
+                return this._removeFileAssociationForMovie(movieID);
+            })
+            .then(() => {
+                return this._moviesModel.deleteSingle(movieID);
+            });
+    }
+
+    _removeShowThumbnail(showID) {
+        return this._showsModel.getSingle(showID).then(show => {
+            const fullPath = path.join(thumbConfig.shows, show.image_filename);
+            if (!fs.existsSync(fullPath)) {
+                return Promise.resolve(true);
+            }
+            return Promise.resolve(fs.unlinkSync(fullPath));
+        });
+    }
+
+    _removeFileAssociationForMovie(movieID) {
+        return this._fileToMovieModel
+            .getSingleForMovie(movieID)
+            .then(fileToMovie => {
+                if (!fileToMovie.hasOwnProperty("file_id")) {
+                    // No file to remove
+                    return Promise.resolve();
+                }
+
+                return this._filesModel
+                    .deleteSingle(fileToMovie.file_id)
+                    .then(() => {
+                        return this._fileToMovieModel.deleteSingle(
+                            fileToMovie.file_id,
+                            movieID
+                        );
+                    });
+            });
+    }
+
+    _removeMovieThumbnail(movie) {
         const fullPath = path.join(thumbConfig.movies, movie.image_filename);
-        if(!fs.existsSync(fullPath)){
+        if (!fs.existsSync(fullPath)) {
             return Promise.resolve(true);
         }
         return Promise.resolve(fs.unlinkSync(fullPath));
