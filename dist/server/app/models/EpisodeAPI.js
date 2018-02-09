96796ef05930bf47a4d0242d98ae6aebfe0babd0
Destin Moulton <destin.moulton@gmail.com>
2018-02-09 13:20:33 -0700

    Refactored the file collection process into its own method.

--- dist/server/app/models/EpisodeAPI.js
+++ dist/server/app/models/EpisodeAPI.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
     value: true
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _logger = require('../logger');
+var _logger = require("../logger");
 
 var _logger2 = _interopRequireDefault(_logger);
 
@@ -24,7 +24,7 @@ var EpisodeAPI = function () {
     }
 
     _createClass(EpisodeAPI, [{
-        key: 'getAllEpisodesWithFileInfo',
+        key: "getAllEpisodesWithFileInfo",
         value: function getAllEpisodesWithFileInfo(showID, seasonNum) {
             var _this = this;
 
@@ -33,24 +33,38 @@ var EpisodeAPI = function () {
 
                 episodes.forEach(function (ep) {
                     var data = Object.assign({}, ep);
-                    data['file_info'] = {};
+                    data["file_info"] = {};
 
                     var cmd = _this._fileToEpisodeModel.getSingleForEpisode(ep.id).then(function (fileEp) {
-                        if (!fileEp.hasOwnProperty('file_id')) {
+                        if (!fileEp.hasOwnProperty("file_id")) {
                             return Promise.resolve(data);
                         }
-                        return _this._filesModel.getSingle(fileEp.file_id).then(function (file) {
-                            if (!file.hasOwnProperty('id')) {
-                                return Promise.resolve(data);
-                            }
-                            data.file_info = file;
-                            return Promise.resolve(data);
-                        });
+
+                        return _this._collectFileInfo(fileEp, data);
                     });
                     promisesToRun.push(cmd);
                 });
 
                 return Promise.all(promisesToRun);
+            });
+        }
+
+        /**
+         * Add File info to the episode object if it exists.
+         *
+         * @param FileToEpisode fileEp
+         * @param ShowSeasonEpisode episode
+         */
+
+    }, {
+        key: "_collectFileInfo",
+        value: function _collectFileInfo(fileEp, episode) {
+            return this._filesModel.getSingle(fileEp.file_id).then(function (file) {
+                if (!file.hasOwnProperty("id")) {
+                    return Promise.resolve(episode);
+                }
+                episode.file_info = file;
+                return Promise.resolve(episode);
             });
         }
     }]);
--- src/server/models/EpisodeAPI.js
+++ src/server/models/EpisodeAPI.js
@@ -1,40 +1,51 @@
-import logger from '../logger';
+import logger from "../logger";
 
 export default class EpisodeAPI {
-
-    constructor(models){
+    constructor(models) {
         this._showSeasonEpisodesModel = models.showSeasonEpisodesModel;
         this._filesModel = models.filesModel;
         this._fileToEpisodeModel = models.fileToEpisodeModel;
     }
 
-    getAllEpisodesWithFileInfo(showID, seasonNum){
-        return this._showSeasonEpisodesModel.getEpisodesForSeasonNum(showID, seasonNum)
-                .then((episodes)=>{
-                    let promisesToRun = [];
-                    
-                    episodes.forEach((ep)=>{
-                        let data = Object.assign({}, ep);
-                        data['file_info'] = {};
-                        
-                        const cmd = this._fileToEpisodeModel.getSingleForEpisode(ep.id)
-                                        .then((fileEp)=>{
-                                            if(!fileEp.hasOwnProperty('file_id')){
-                                                return Promise.resolve(data);
-                                            }
-                                            return this._filesModel.getSingle(fileEp.file_id)
-                                                    .then((file)=>{
-                                                        if(!file.hasOwnProperty('id')){
-                                                            return Promise.resolve(data);
-                                                        }
-                                                        data.file_info = file;
-                                                        return Promise.resolve(data);
-                                                    });
-                                        });
-                        promisesToRun.push(cmd);
-                    });
+    getAllEpisodesWithFileInfo(showID, seasonNum) {
+        return this._showSeasonEpisodesModel
+            .getEpisodesForSeasonNum(showID, seasonNum)
+            .then(episodes => {
+                let promisesToRun = [];
 
-                    return Promise.all(promisesToRun);
+                episodes.forEach(ep => {
+                    let data = Object.assign({}, ep);
+                    data["file_info"] = {};
+
+                    const cmd = this._fileToEpisodeModel
+                        .getSingleForEpisode(ep.id)
+                        .then(fileEp => {
+                            if (!fileEp.hasOwnProperty("file_id")) {
+                                return Promise.resolve(data);
+                            }
+
+                            return this._collectFileInfo(fileEp, data);
+                        });
+                    promisesToRun.push(cmd);
                 });
+
+                return Promise.all(promisesToRun);
+            });
+    }
+
+    /**
+     * Add File info to the episode object if it exists.
+     *
+     * @param FileToEpisode fileEp
+     * @param ShowSeasonEpisode episode
+     */
+    _collectFileInfo(fileEp, episode) {
+        return this._filesModel.getSingle(fileEp.file_id).then(file => {
+            if (!file.hasOwnProperty("id")) {
+                return Promise.resolve(episode);
+            }
+            episode.file_info = file;
+            return Promise.resolve(episode);
+        });
     }
 }
