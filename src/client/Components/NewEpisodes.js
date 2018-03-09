7654bf2094976646c16792a23bac157108e5b242
Destin Moulton <destin.moulton@gmail.com>
2018-03-09 16:05:23 -0700

    Changed new episode style.

--- src/client/Components/NewEpisodes.js
+++ src/client/Components/NewEpisodes.js
@@ -1,27 +1,27 @@
-import moment from 'moment';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { truncate } from 'lodash';
+import moment from "moment";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { truncate } from "lodash";
 
-import { Col, Icon, Row, Spin } from 'antd';
+import { Col, Icon, Row, Spin } from "antd";
 
-import { emitAPIRequest } from '../actions/api.actions';
+import { emitAPIRequest } from "../actions/api.actions";
 
 class NewEpisodes extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
             days: [],
             episodes: {}
-        }
+        };
     }
 
-    componentWillMount(){
+    componentWillMount() {
         let days = [];
-        for(let i=0; i<=7; i++){
-            const dayMoment = moment().subtract(i, 'day')
+        for (let i = 0; i <= 7; i++) {
+            const dayMoment = moment().subtract(i, "day");
             days.push(dayMoment);
             this._getNewEpisodes(dayMoment);
         }
@@ -31,20 +31,33 @@ class NewEpisodes extends Component {
         });
     }
 
-    _getNewEpisodes(dayMoment){
+    _getNewEpisodes(dayMoment) {
         const { emitAPIRequest } = this.props;
 
-        const startUnixTimestamp = dayMoment.hours(0).minutes(0).seconds(0).unix();
-        const endUnixTimestamp = dayMoment.hours(23).minutes(59).seconds(59).unix();
+        const startUnixTimestamp = dayMoment
+            .hours(0)
+            .minutes(0)
+            .seconds(0)
+            .unix();
+        const endUnixTimestamp = dayMoment
+            .hours(23)
+            .minutes(59)
+            .seconds(59)
+            .unix();
         const params = {
             start_unix_timestamp: startUnixTimestamp,
             end_unix_timestamp: endUnixTimestamp
         };
 
-        emitAPIRequest("shows.episodes.get_between_unix_timestamps", params, this._episodesReceived.bind(this, dayMoment), false);
+        emitAPIRequest(
+            "shows.episodes.get_between_unix_timestamps",
+            params,
+            this._episodesReceived.bind(this, dayMoment),
+            false
+        );
     }
 
-    _episodesReceived(dayMoment, newEpisodes){
+    _episodesReceived(dayMoment, newEpisodes) {
         let { episodes } = this.state;
 
         episodes[dayMoment.unix()] = newEpisodes;
@@ -54,68 +67,97 @@ class NewEpisodes extends Component {
         });
     }
 
-    _buildEpisodeDetails(episode){
+    _buildEpisodeDetails(episode) {
         return (
-            <div key={episode.id}
-                    className="ib-newepisode-container">
+            <div key={episode.id} className="ib-newepisode-container">
                 <div className="ib-newepisode-img-box">
-                    <Link to={"/show/"+episode.show_info.slug}>
+                    <Link to={"/show/" + episode.show_info.slug}>
                         <img
                             className="ib-newepisode-img"
-                            src={"/images/shows/" + episode.show_info.image_filename}
+                            src={
+                                "/images/shows/" +
+                                episode.show_info.image_filename
+                            }
                         />
                     </Link>
                 </div>
                 <div className="ib-newepisode-details-box">
                     <div className="ib-newepisode-show-title">
-                        <Link to={"/show/"+episode.show_info.slug}>
-                            {truncate(episode.show_info.title, {length:32})}
+                        <Link to={"/show/" + episode.show_info.slug}>
+                            {truncate(episode.show_info.title, { length: 50 })}
                         </Link>
                     </div>
                     <div className="ib-newepisode-ep-title">
-                        {truncate(episode.title, {length:35})}
+                        {truncate(episode.title, { length: 35 })}
                     </div>
                     <div>
-                        Season {episode.season_number} - Episode {episode.episode_number}
+                        Season {episode.season_number} - Episode{" "}
+                        {episode.episode_number}
                     </div>
                     <div className="ib-newepisode-links">
-                        <a target="_blank" href={"https://www.1337x.to/search/" + encodeURIComponent(episode.show_info.title)+"/1/"}>1337x</a>
+                        <a
+                            target="_blank"
+                            href={
+                                "https://www.1337x.to/search/" +
+                                encodeURIComponent(episode.show_info.title) +
+                                "/1/"
+                            }
+                        >
+                            1337x
+                        </a>
                         &nbsp;|&nbsp;
-                        <a target="_blank" href={"https://thepiratebay.org/search/" + encodeURIComponent(episode.show_info.title)+"/0/99/0/"}>thepiratebay</a>
+                        <a
+                            target="_blank"
+                            href={
+                                "https://thepiratebay.org/search/" +
+                                encodeURIComponent(episode.show_info.title) +
+                                "/0/99/0/"
+                            }
+                        >
+                            thepiratebay
+                        </a>
                     </div>
                 </div>
             </div>
         );
     }
 
-    _buildEpisodesList(){
+    _buildEpisodesList() {
         const { days, episodes } = this.state;
 
         let elements = [];
-        days.forEach((dayMoment)=>{
+        days.forEach(dayMoment => {
             let unixDay = dayMoment.unix();
-            const header = <h3><Icon type="calendar"/>&nbsp;{dayMoment.format("dddd, MMMM Do YYYY")}</h3>;
+            const header = (
+                <h3>
+                    <Icon type="calendar" />&nbsp;{dayMoment.format(
+                        "dddd, MMMM Do YYYY"
+                    )}
+                </h3>
+            );
 
             let epList = [];
-            if(episodes.hasOwnProperty(unixDay)){
-                if(episodes[unixDay].length === 0){
+            if (episodes.hasOwnProperty(unixDay)) {
+                if (episodes[unixDay].length === 0) {
                     // No episodes message
-                    epList = <div key={unixDay}
-                                  className="ib-newepisode-container">
-                                <h3>Nothing to see here...</h3>
-                            </div>;
+                    epList = (
+                        <div key={unixDay} className="ib-newepisode-container">
+                            <h3>Nothing to see here...</h3>
+                        </div>
+                    );
                 } else {
-                    episodes[unixDay].forEach((episode)=>{
+                    episodes[unixDay].forEach(episode => {
                         const epEl = this._buildEpisodeDetails(episode);
                         epList.push(epEl);
                     });
                 }
             }
-            const el = <Row key={unixDay}
-                            className="ib-newepisode-section">
-                            {header}
-                            {epList}
-                        </Row>;
+            const el = (
+                <Row key={unixDay} className="ib-newepisode-section">
+                    {header}
+                    {epList}
+                </Row>
+            );
             elements.push(el);
         });
         return elements;
@@ -123,22 +165,19 @@ class NewEpisodes extends Component {
 
     render() {
         const episodesList = this._buildEpisodesList();
-        return (
-            <div>
-                {episodesList}
-            </div>
-        );
+        return <div>{episodesList}</div>;
     }
 }
 
-const mapStateToProps = (state)=>{
+const mapStateToProps = state => {
     return {};
-}
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
     return {
-        emitAPIRequest: (endpoint, params, callback, shouldDispatch)=>dispatch(emitAPIRequest(endpoint, params, callback, shouldDispatch))
-    }
-}
+        emitAPIRequest: (endpoint, params, callback, shouldDispatch) =>
+            dispatch(emitAPIRequest(endpoint, params, callback, shouldDispatch))
+    };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewEpisodes);
