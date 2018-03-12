31c9702ec9e28a154c3516d5cda79dbe7e4b8c41
Destin Moulton <destin.moulton@gmail.com>
2018-03-12 16:29:32 -0600

    Added search/enterButton to the search box.

--- src/client/Components/ID/MovieCheckForm.js
+++ src/client/Components/ID/MovieCheckForm.js
@@ -1,10 +1,7 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
 
-import { 
-    Checkbox,
-    Input
-} from 'antd';
+import { Checkbox, Input } from "antd";
 
 class MovieCheckForm extends Component {
     static propTypes = {
@@ -12,7 +9,7 @@ class MovieCheckForm extends Component {
         onSearchMovies: PropTypes.func.isRequired
     };
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -21,11 +18,11 @@ class MovieCheckForm extends Component {
         };
     }
 
-    _handleClickMovieCheck(name){
+    _handleClickMovieCheck(name) {
         const { checkedMovieNames } = this.state;
 
         const possibleNameIndex = checkedMovieNames.indexOf(name);
-        if(possibleNameIndex > -1){
+        if (possibleNameIndex > -1) {
             checkedMovieNames.splice(possibleNameIndex, 1);
         } else {
             checkedMovieNames.push(name);
@@ -37,52 +34,86 @@ class MovieCheckForm extends Component {
         });
     }
 
-    _handleChangeSearchText(evt){
+    _handleChangeSearchText(evt) {
         this.setState({
             searchString: evt.target.value
-        })
+        });
     }
 
-    _movieFilenameToCleanArray(filename){
-        const unwantedFormats = ['avi', 'ac3', 'aac', 'hevc', 'mkv', 'mp3', 'mp4', 'x264', 'x265'];
-        const unwantedMedia = ['bluray', 'brrip', 'dvd', 'dvdr', 'dvdscr', 'hdrip', 'web', 'xvid'];
-        const unwantedRes = ['480', '480p', '720', '720p', '1080', '1080p', '4k'];
+    _movieFilenameToCleanArray(filename) {
+        const unwantedFormats = [
+            "avi",
+            "ac3",
+            "aac",
+            "hevc",
+            "mkv",
+            "mp3",
+            "mp4",
+            "x264",
+            "x265"
+        ];
+        const unwantedMedia = [
+            "bluray",
+            "brrip",
+            "dvd",
+            "dvdr",
+            "dvdscr",
+            "hdrip",
+            "web",
+            "xvid"
+        ];
+        const unwantedRes = [
+            "480",
+            "480p",
+            "720",
+            "720p",
+            "1080",
+            "1080p",
+            "4k"
+        ];
         const unwanted = [...unwantedFormats, ...unwantedMedia, ...unwantedRes];
 
         // Make spaces into periods and split on periods
         const parts = filename
-                        .replace(/[^\w]/g, " ")
-                        .replace(/(\s+)/g, ".")
-                        .split(".");
+            .replace(/[^\w]/g, " ")
+            .replace(/(\s+)/g, ".")
+            .split(".");
 
-        return parts.filter((part)=>{
+        return parts.filter(part => {
             return unwanted.indexOf(part.toLowerCase()) < 0;
-        });;
+        });
     }
 
     render() {
         const { currentFilename, onSearchMovies } = this.props;
         const { checkedMovieNames, searchString } = this.state;
-        
+
         const possibleNames = this._movieFilenameToCleanArray(currentFilename);
 
         let possibleChecks = [];
-        possibleNames.forEach((name)=>{
-            const checkBox = <div key={name} ><Checkbox onChange={this._handleClickMovieCheck.bind(this,name)}>{name}</Checkbox></div>
+        possibleNames.forEach(name => {
+            const checkBox = (
+                <div key={name}>
+                    <Checkbox
+                        onChange={this._handleClickMovieCheck.bind(this, name)}
+                    >
+                        {name}
+                    </Checkbox>
+                </div>
+            );
             possibleChecks.push(checkBox);
         });
 
         return (
             <div>
-                <Input.Search 
-                    placeholder="Movie search..." 
+                <Input.Search
+                    placeholder="Movie search..."
                     value={searchString}
                     onSearch={onSearchMovies}
                     onChange={this._handleChangeSearchText.bind(this)}
+                    enterButton
                 />
-                <div>
-                    {possibleChecks}
-                </div>
+                <div>{possibleChecks}</div>
             </div>
         );
     }
