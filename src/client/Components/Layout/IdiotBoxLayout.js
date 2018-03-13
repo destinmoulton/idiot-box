7550f01ec38be02f2d5ac3281b9dac4aedfb3f3a
Destin Moulton <destin.moulton@gmail.com>
2018-03-13 16:50:16 -0600

    Removed style override; added id.

--- src/client/Components/Layout/IdiotBoxLayout.js
+++ src/client/Components/Layout/IdiotBoxLayout.js
@@ -1,36 +1,44 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import { Layout, Menu } from 'antd';
+import { Layout, Menu } from "antd";
 const { Header, Content, Footer } = Layout;
 
-import FileManager from '../FileManager';
-import IdiotBoxHeader from './IdiotBoxHeader';
-import MoviesList from '../Movies/MoviesList';
-import NewEpisodes from '../NewEpisodes';
-import Settings from '../Settings/Settings';
-import ShowInfo from '../Shows/ShowInfo';
-import ShowsList from '../Shows/ShowsList';
+import FileManager from "../FileManager";
+import IdiotBoxHeader from "./IdiotBoxHeader";
+import MoviesList from "../Movies/MoviesList";
+import NewEpisodes from "../NewEpisodes";
+import Settings from "../Settings/Settings";
+import ShowInfo from "../Shows/ShowInfo";
+import ShowsList from "../Shows/ShowsList";
 
 class IdiotBoxLayout extends Component {
-    
     render() {
         return (
             <BrowserRouter>
                 <Layout className="layout">
                     <IdiotBoxHeader />
-                    <Content style={{ padding: '0 50px' }}>
-                    <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-                        <Route path='/newepisodes' component={NewEpisodes}/>
-                        <Route path='/filemanager/:setting_key?/:subpath?' component={FileManager}/>
-                        <Route path='/movies' component={MoviesList}/>
-                        <Route path='/settings' component={Settings}/>
-                        <Route path='/shows' component={ShowsList}/>
-                        <Route path={`/show/:slug/:season_id?`} component={ShowInfo}/>
-                    </div>
+                    <Content style={{ padding: "0 50px" }}>
+                        <div id="ib-layout-content-box">
+                            <Route
+                                path="/newepisodes"
+                                component={NewEpisodes}
+                            />
+                            <Route
+                                path="/filemanager/:setting_key?/:subpath?"
+                                component={FileManager}
+                            />
+                            <Route path="/movies" component={MoviesList} />
+                            <Route path="/settings" component={Settings} />
+                            <Route path="/shows" component={ShowsList} />
+                            <Route
+                                path={`/show/:slug/:season_id?`}
+                                component={ShowInfo}
+                            />
+                        </div>
                     </Content>
-                    <Footer style={{ textAlign: 'center' }}>
-                    Idiot Box created by Destin Moulton
+                    <Footer style={{ textAlign: "center" }}>
+                        Idiot Box created by Destin Moulton
                     </Footer>
                 </Layout>
             </BrowserRouter>
