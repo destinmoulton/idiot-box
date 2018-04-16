a78347571b0f055d623bbecbcb985d53d23a02f3
Destin Moulton <destin.moulton@gmail.com>
2018-04-16 14:00:39 -0600

    Converted to React function component.

--- src/client/Components/Filesystem/DirectorySelectorModal.js
+++ src/client/Components/Filesystem/DirectorySelectorModal.js
@@ -1,73 +1,67 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 
 import { Modal } from "antd";
 
 import FilesystemBrowser from "./FilesystemBrowser";
 
-class DirectorySelectorModal extends Component {
-    static propTypes = {
-        initialPath: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        visible: PropTypes.bool,
-        onCancel: PropTypes.func,
-        onChangeDirectory: PropTypes.func.isRequired,
-        onOk: PropTypes.func.isRequired
+const DirectorySelectorModal = props => {
+    const {
+        initialPath,
+        onCancel,
+        onChangeDirectory,
+        onOk,
+        title,
+        visible
+    } = props;
+
+    const posDim = {
+        modalTop: 30,
+        modalHeight: window.innerHeight - 180,
+        fileBrowserHeight: window.innerHeight - 200
     };
 
-    static defaultProps = {
-        title: "Select Directory",
-        visible: false,
-        onChangeDirectory: () => {},
-        onCancel: () => {}
-    };
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const {
-            initialPath,
-            onCancel,
-            onChangeDirectory,
-            onOk,
-            title,
-            visible
-        } = this.props;
-
-        const posDim = {
-            modalTop: 30,
-            modalHeight: window.innerHeight - 180,
-            fileBrowserHeight: window.innerHeight - 200
-        };
-
-        return (
-            <div>
-                <Modal
-                    title={title}
-                    visible={visible}
-                    onCancel={onCancel}
-                    onOk={onOk}
-                    okText="Choose This Directory"
-                    cancelText="Cancel"
-                    style={{ top: posDim.modalTop, height: posDim.modalHeight }}
+    return (
+        <div>
+            <Modal
+                title={title}
+                visible={visible}
+                onCancel={onCancel}
+                onOk={onOk}
+                okText="Choose This Directory"
+                cancelText="Cancel"
+                style={{ top: posDim.modalTop, height: posDim.modalHeight }}
+            >
+                <div
+                    className="ib-settings-dirsel-fs-cont"
+                    style={{ height: posDim.fileBrowserHeight }}
                 >
-                    <div
-                        className="ib-settings-dirsel-fs-cont"
-                        style={{ height: posDim.fileBrowserHeight }}
-                    >
-                        <FilesystemBrowser
-                            basePath={initialPath}
-                            onChangeDirectory={onChangeDirectory}
-                            showDirectories={true}
-                            showFiles={false}
-                        />
-                    </div>
-                </Modal>
-            </div>
-        );
-    }
-}
+                    <FilesystemBrowser
+                        basePath={initialPath}
+                        onChangeDirectory={onChangeDirectory}
+                        showDirectories={true}
+                        showFiles={false}
+                    />
+                </div>
+            </Modal>
+        </div>
+    );
+};
+
+DirectorySelectorModal.propTypes = {
+    initialPath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    visible: PropTypes.bool,
+    onCancel: PropTypes.func,
+    onChangeDirectory: PropTypes.func.isRequired,
+    onOk: PropTypes.func.isRequired
+};
+
+DirectorySelectorModal.defaultProps = {
+    title: "Select Directory",
+    visible: false,
+    onChangeDirectory: () => {},
+    onCancel: () => {}
+};
 
 export default DirectorySelectorModal;
