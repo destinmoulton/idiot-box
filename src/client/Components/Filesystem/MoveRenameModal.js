53b069bfbb710e685e861fb7addab0d6c473c78c
Destin Moulton <destin.moulton@gmail.com>
2018-04-16 14:12:29 -0600

    Migrated from redux connect to using callAPI prop.

--- src/client/Components/Filesystem/MoveRenameModal.js
+++ src/client/Components/Filesystem/MoveRenameModal.js
@@ -1,37 +1,29 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import { Col, Input, Modal } from 'antd';
+import { Col, Input, Modal } from "antd";
 
-import FilesystemBrowser from './FilesystemBrowser';
+import FilesystemBrowser from "./FilesystemBrowser";
 
-import { callAPI } from '../../actions/api.actions';
+import { callAPI } from "../../actions/api.actions";
 
 class MoveRenameModal extends Component {
-    static propTypes = {
-        initialPath: PropTypes.string.isRequired,
-        isVisible: PropTypes.bool.isRequired,
-        itemsToRename: PropTypes.array.isRequired,
-        onCancel: PropTypes.func.isRequired,
-        onRenameComplete: PropTypes.func.isRequired
-    };
-
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
             destinationPath: "",
             isRenaming: false,
-            itemsRenamed: {},
+            itemsRenamed: {}
         };
     }
 
-    componentWillReceiveProps(nextProps){
+    componentWillReceiveProps(nextProps) {
         const { itemsToRename } = nextProps;
-        
+
         let itemsRenamed = {};
-        itemsToRename.forEach((item)=>{
+        itemsToRename.forEach(item => {
             itemsRenamed[item] = item;
         });
 
@@ -40,11 +32,11 @@ class MoveRenameModal extends Component {
         });
     }
 
-    _handleClickRename(){
+    _handleClickRename() {
         this._performRename();
     }
 
-    _performRename(){
+    _performRename() {
         const { callAPI, initialPath } = this.props;
         const { destinationPath, itemsRenamed } = this.state;
 
@@ -58,10 +50,15 @@ class MoveRenameModal extends Component {
             items_to_rename: itemsRenamed
         };
 
-        callAPI("filesystem.rename.multiple", params, this._renameComplete.bind(this), false);
+        callAPI(
+            "filesystem.rename.multiple",
+            params,
+            this._renameComplete.bind(this),
+            false
+        );
     }
 
-    _renameComplete(){
+    _renameComplete() {
         const { onRenameComplete } = this.props;
 
         this.setState({
@@ -71,13 +68,13 @@ class MoveRenameModal extends Component {
         onRenameComplete();
     }
 
-    _handleChangeDirectory(newPath){
+    _handleChangeDirectory(newPath) {
         this.setState({
             destinationPath: newPath
         });
     }
 
-    _handleChangeFilename(originalFilename, evt){
+    _handleChangeFilename(originalFilename, evt) {
         const { itemsRenamed } = this.state;
 
         itemsRenamed[originalFilename] = evt.target.value;
@@ -87,18 +84,23 @@ class MoveRenameModal extends Component {
         });
     }
 
-    _buildRenameInputs(){
+    _buildRenameInputs() {
         const { itemsToRename } = this.props;
         const { itemsRenamed } = this.state;
 
         let inputList = [];
-        itemsToRename.forEach((item)=>{
-            const el = <div key={item}
-                            className="ib-moverename-input-box">
-                            <h5><u>Original:</u> {item}</h5>
-                            <Input onChange={this._handleChangeFilename.bind(this, item)}
-                                   value={itemsRenamed[item]}/>
-                       </div>
+        itemsToRename.forEach(item => {
+            const el = (
+                <div key={item} className="ib-moverename-input-box">
+                    <h5>
+                        <u>Original:</u> {item}
+                    </h5>
+                    <Input
+                        onChange={this._handleChangeFilename.bind(this, item)}
+                        value={itemsRenamed[item]}
+                    />
+                </div>
+            );
             inputList.push(el);
         });
 
@@ -106,19 +108,15 @@ class MoveRenameModal extends Component {
     }
 
     render() {
-        const {
-            initialPath,
-            isVisible,
-            onCancel
-        } = this.props;
+        const { initialPath, isVisible, onCancel } = this.props;
 
         const posDim = {
             modalTop: 30,
             modalHeight: window.innerHeight - 180,
             modalWidth: window.innerWidth - 100,
             fileBrowserHeight: window.innerHeight - 450
-        }
-        
+        };
+
         const inputBoxes = this._buildRenameInputs();
 
         return (
@@ -130,37 +128,41 @@ class MoveRenameModal extends Component {
                     onOk={this._handleClickRename.bind(this)}
                     okText="Move or Rename"
                     cancelText="Cancel"
-                    style={{top: posDim.modalTop, height:posDim.modalHeight}}
-                    width={posDim.modalWidth}>
-                        <div className="ib-moverename-dirsel-box" 
-                            style={{height:posDim.fileBrowserHeight}}>
-                            <h4>Destination Directory</h4>
-                            <FilesystemBrowser 
-                                basePath={initialPath}
-                                lockToBasePath={false}
-                                onChangeDirectory={this._handleChangeDirectory.bind(this)}
-                                showDirectories={true}
-                                showFiles={false}
-                            />
-                        </div>
-                        <div className="ib-moverename-inputs-container">
-                            <h4>Items to Rename</h4>
-                            {inputBoxes}
-                        </div>
+                    style={{ top: posDim.modalTop, height: posDim.modalHeight }}
+                    width={posDim.modalWidth}
+                >
+                    <div
+                        className="ib-moverename-dirsel-box"
+                        style={{ height: posDim.fileBrowserHeight }}
+                    >
+                        <h4>Destination Directory</h4>
+                        <FilesystemBrowser
+                            basePath={initialPath}
+                            lockToBasePath={false}
+                            onChangeDirectory={this._handleChangeDirectory.bind(
+                                this
+                            )}
+                            showDirectories={true}
+                            showFiles={false}
+                        />
+                    </div>
+                    <div className="ib-moverename-inputs-container">
+                        <h4>Items to Rename</h4>
+                        {inputBoxes}
+                    </div>
                 </Modal>
             </div>
         );
     }
 }
 
-const mapStateToProps = (state)=>{
-    return {};
-}
+MoveRenameModal.propTypes = {
+    callAPI: PropTypes.func.isRequired,
+    initialPath: PropTypes.string.isRequired,
+    isVisible: PropTypes.bool.isRequired,
+    itemsToRename: PropTypes.array.isRequired,
+    onCancel: PropTypes.func.isRequired,
+    onRenameComplete: PropTypes.func.isRequired
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        callAPI: (endpoint, params, callback, shouldDispatch)=>dispatch(callAPI(endpoint, params, callback, shouldDispatch))
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(MoveRenameModal);
+export default MoveRenameModal;
