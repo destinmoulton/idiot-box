7cfa368cd55a3a1f5141e7df55e9f8754f6ec708
Destin Moulton <destin.moulton@gmail.com>
2018-04-16 13:56:47 -0600

    Converted to react function.

--- src/client/Components/Filesystem/FileDetails.js
+++ src/client/Components/Filesystem/FileDetails.js
@@ -1,44 +1,40 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
-import { Icon, Spin } from 'antd';
+import { Icon, Spin } from "antd";
 
-import PlayButton from '../PlayButton';
+import PlayButton from "../PlayButton";
 
-import Regex from '../../lib/Regex.lib';
-class FileDetails extends Component {
-
-    static propTypes = {
-        assocData: PropTypes.object.isRequired,
-        basePath: PropTypes.string.isRequired,
-        filename: PropTypes.string.isRequired,
-        fullPath: PropTypes.string.isRequired
-    }
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { assocData, filename, fullPath } = this.props;
-
-        let mediaDetails = "";
-        if('title' in assocData){
-            mediaDetails = <div className="ib-filebrowser-media-title">{assocData.title}</div>;
-        }
-
-        let actions = "";
-        if(Regex.isVideoFile(filename)){
-            actions = <PlayButton filename={filename} fullPath={fullPath} />;
-        }
-        
-        return (
-            <div>
-                {mediaDetails}
-                <div>{actions}{filename}</div>
-            </div>
+import Regex from "../../lib/Regex.lib";
+const FileDetails = ({ assocData, filename, fullPath }) => {
+    let mediaDetails = "";
+    if ("title" in assocData) {
+        mediaDetails = (
+            <div className="ib-filebrowser-media-title">{assocData.title}</div>
         );
     }
-}
+
+    let actions = "";
+    if (Regex.isVideoFile(filename)) {
+        actions = <PlayButton filename={filename} fullPath={fullPath} />;
+    }
+
+    return (
+        <div>
+            {mediaDetails}
+            <div>
+                {actions}
+                {filename}
+            </div>
+        </div>
+    );
+};
+
+FileDetails.propTypes = {
+    assocData: PropTypes.object.isRequired,
+    basePath: PropTypes.string.isRequired,
+    filename: PropTypes.string.isRequired,
+    fullPath: PropTypes.string.isRequired
+};
 
 export default FileDetails;
