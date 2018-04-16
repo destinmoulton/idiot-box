ee1767869d2b3cf6d36468604cff9f720d3e57fa
Destin Moulton <destin.moulton@gmail.com>
2018-04-16 13:36:41 -0600

    Added redux mapping for state and dispatch; migrated MoviesList component to props.

--- src/client/Components/Layout/IdiotBoxLayout.js
+++ src/client/Components/Layout/IdiotBoxLayout.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { BrowserRouter, Redirect, Route } from "react-router-dom";
 
 import { Layout, Menu } from "antd";
 const { Header, Content, Footer } = Layout;
+
+import { callAPI } from "../../actions/api.actions";
 
 import FileManager from "../FileManager";
 import IdiotBoxHeader from "./IdiotBoxHeader";
@@ -12,39 +15,56 @@ import Settings from "../Settings/Settings";
 import ShowInfo from "../Shows/ShowInfo";
 import ShowsList from "../Shows/ShowsList";
 
-class IdiotBoxLayout extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <Layout className="layout">
-                    <IdiotBoxHeader />
-                    <Content style={{ padding: "0 50px" }}>
-                        <div id="ib-layout-content-box">
-                            <Route path="/" exact component={NewEpisodes} />
-                            <Route
-                                path="/newepisodes"
-                                component={NewEpisodes}
-                            />
-                            <Route
-                                path="/filemanager/:setting_key?/:subpath?"
-                                component={FileManager}
-                            />
-                            <Route path="/movies" component={MoviesList} />
-                            <Route path="/settings" component={Settings} />
-                            <Route path="/shows" component={ShowsList} />
-                            <Route
-                                path={`/show/:slug/:season_number?`}
-                                component={ShowInfo}
-                            />
-                        </div>
-                    </Content>
-                    <Footer style={{ textAlign: "center" }}>
-                        Idiot Box created by Destin Moulton
-                    </Footer>
-                </Layout>
-            </BrowserRouter>
-        );
-    }
-}
+const IdiotBoxLayout = props => {
+    return (
+        <BrowserRouter>
+            <Layout className="layout">
+                <IdiotBoxHeader />
+                <Content style={{ padding: "0 50px" }}>
+                    <div id="ib-layout-content-box">
+                        <Route path="/" exact component={NewEpisodes} />
+                        <Route path="/newepisodes" component={NewEpisodes} />
+                        <Route
+                            path="/filemanager/:setting_key?/:subpath?"
+                            component={FileManager}
+                        />
+                        <Route
+                            path="/movies"
+                            render={() => (
+                                <MoviesList
+                                    settings={props.settings}
+                                    callAPI={props.callAPI}
+                                />
+                            )}
+                        />
+                        <Route path="/settings" component={Settings} />
+                        <Route path="/shows" component={ShowsList} />
+                        <Route
+                            path={`/show/:slug/:season_number?`}
+                            component={ShowInfo}
+                        />
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: "center" }}>
+                    Idiot Box created by Destin Moulton
+                </Footer>
+            </Layout>
+        </BrowserRouter>
+    );
+};
 
-export default IdiotBoxLayout;
+const mapStateToProps = state => {
+    const { settings } = state;
+    return {
+        settings: settings.settings
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        callAPI: (endpoint, params, callback, shouldDispatch) =>
+            dispatch(callAPI(endpoint, params, callback, shouldDispatch))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(IdiotBoxLayout);
