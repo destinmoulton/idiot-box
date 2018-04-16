af878e70d1cf42fd0e289d0d846259f2e4426380
Destin Moulton <destin.moulton@gmail.com>
2018-04-16 14:15:13 -0600

    Converted to redux function component.

--- src/client/Components/Shows/AddShowModal.js
+++ src/client/Components/Shows/AddShowModal.js
@@ -1,58 +1,52 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 
 import { Button, Col, Input, Modal, Select } from "antd";
 
 import ShowResults from "./ShowResults";
 
-class AddShowModal extends Component {
-    static propTypes = {
-        currentSearchString: PropTypes.string.isRequired,
-        isVisible: PropTypes.bool.isRequired,
-        onAddShowComplete: PropTypes.func.isRequired,
-        onCancel: PropTypes.func.isRequired
-    };
+const AddShowModal = props => {
+    const {
+        currentSearchString,
+        isVisible,
+        onAddShowComplete,
+        onCancel
+    } = props;
 
-    constructor(props) {
-        super(props);
-    }
+    let showResults = null;
 
-    render() {
-        const {
-            currentSearchString,
-            isVisible,
-            onAddShowComplete,
-            onCancel
-        } = this.props;
-
-        let showResults = null;
-
-        if (isVisible) {
-            showResults = (
-                <ShowResults
-                    currentSearchString={currentSearchString}
-                    onAddShowComplete={onAddShowComplete}
-                />
-            );
-        }
-
-        return (
-            <Modal
-                title="Add New Show"
-                visible={isVisible}
-                onCancel={onCancel}
-                footer={[
-                    <Button key="cancel" size="large" onClick={onCancel}>
-                        Cancel
-                    </Button>
-                ]}
-                width={700}
-            >
-                {showResults}
-            </Modal>
+    if (isVisible) {
+        showResults = (
+            <ShowResults
+                currentSearchString={currentSearchString}
+                onAddShowComplete={onAddShowComplete}
+            />
         );
     }
-}
+
+    return (
+        <Modal
+            title="Add New Show"
+            visible={isVisible}
+            onCancel={onCancel}
+            footer={[
+                <Button key="cancel" size="large" onClick={onCancel}>
+                    Cancel
+                </Button>
+            ]}
+            width={700}
+        >
+            {showResults}
+        </Modal>
+    );
+};
+
+AddShowModal.propTypes = {
+    currentSearchString: PropTypes.string.isRequired,
+    isVisible: PropTypes.bool.isRequired,
+    onAddShowComplete: PropTypes.func.isRequired,
+    onCancel: PropTypes.func.isRequired
+};
 
 export default AddShowModal;
