62c9cb7c3d0ab1a3893ec1a51c0ac5b255da2256
Destin Moulton <destin.moulton@gmail.com>
2021-01-30 11:40:17 -0700

    Changed the scraper to use needle rather than request.

--- src/server/models/IMDBScraperModel.js
+++ src/server/models/IMDBScraperModel.js
@@ -1,4 +1,4 @@
-import rp from "request-promise";
+import needle from "needle";
 import cheerio from "cheerio";
 import randomUseragent from "random-useragent";
 
@@ -11,29 +11,28 @@ export default class IMDBScraperModel {
     getPosterURL(imdbID) {
         const url = this._buildImdbPath(imdbID);
         const options = {
-            url,
-
             headers: {
-                "User-Agent": randomUseragent.getRandom(ua => {
+                "User-Agent": randomUseragent.getRandom((ua) => {
                     return (
                         parseFloat(ua.browserVersion) >= 20 &&
                         ua.browserName === "Firefox"
                     );
                 }),
-                accept: "text/html"
+                accept: "text/html",
             },
-            transform: function(body) {
-                return cheerio.load(body);
-            }
         };
-        return rp(options)
-            .then($ => {
+        return needle
+            .get(url, options)
+            .then((resp) => {
+                return cheerio.load(resp.body);
+            })
+            .then(($) => {
                 return $(this._posterSelector).attr("src");
             })
-            .then(parsedResult => {
+            .then((parsedResult) => {
                 return {
                     imageURL: parsedResult,
-                    url
+                    url,
                 };
             });
     }
