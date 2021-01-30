cf81ba42a0ef2e06962145f051f19eec821c74f0
Destin Moulton <destin.moulton@gmail.com>
2021-01-30 12:35:44 -0700

    Replaced needle with got.

--- src/server/models/IMDBScraperModel.js
+++ src/server/models/IMDBScraperModel.js
@@ -1,30 +1,35 @@
-import needle from "needle";
+import got from "got";
 import cheerio from "cheerio";
 import randomUseragent from "random-useragent";
 
 export default class IMDBScraperModel {
     constructor() {
         this._posterSelector = "div.poster > a > img";
-        this._imdbPath = "http://www.imdb.com/title/";
+        this._imdbPath = "https://www.imdb.com/title/";
     }
 
     getPosterURL(imdbID) {
         const url = this._buildImdbPath(imdbID);
         const options = {
+            retry: 0,
             headers: {
-                "User-Agent": randomUseragent.getRandom((ua) => {
+                "user-agent": randomUseragent.getRandom((ua) => {
                     return (
                         parseFloat(ua.browserVersion) >= 20 &&
                         ua.browserName === "Firefox"
                     );
                 }),
                 accept: "text/html",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "no-cache",
+                pragma: "no-cache",
+                "upgrade-insecure-requests": 1,
             },
         };
-        return needle("get", url, options).then((resp) => {
+        return got.get(url, options).then((resp) => {
             const $ = cheerio.load(resp.body);
             const imageURL = $(this._posterSelector).attr("src");
-
+            console.log(resp);
             return {
                 imageURL,
                 url,
