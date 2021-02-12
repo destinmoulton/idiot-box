5f278f48b5cc7fec662fb9bf01b1c8d2a412378e
Destin Moulton <destin.moulton@gmail.com>
2021-02-12 14:17:04 -0700

    Fixed hamburger menu in header.

--- src/client/Components/Layout/IdiotBoxHeader.js
+++ src/client/Components/Layout/IdiotBoxHeader.js
@@ -3,25 +3,55 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import Toolbar from "@material-ui/core/Toolbar";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListIcon from "@material-ui/icons/List";
+import MenuIcon from "@material-ui/icons/Menu";
+import MovieIcon from "@material-ui/icons/Movie";
+import NewReleasesIcon from "@material-ui/icons/NewReleases";
 import SettingsIcon from "@material-ui/icons/Settings";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import TvIcon from "@material-ui/icons/Tv";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
 const navbarItems = [
-    { path: "/newepisodes", title: "New Episodes" },
-    { path: "/filemanager/Downloads", title: "File Manager" },
-    { path: "/shows", title: "Shows" },
-    { path: "/movies", title: "Movies" },
+    { path: "/newepisodes", title: "New Episodes", icon: <NewReleasesIcon /> },
+    {
+        path: "/filemanager/Downloads",
+        title: "File Manager",
+        icon: <ListIcon />,
+    },
+    { path: "/shows", title: "Shows", icon: <TvIcon /> },
+    { path: "/movies", title: "Movies", icon: <MovieIcon /> },
 ];
 class IdiotBoxHeader extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isDrawerOpen: false,
+        };
+
+        this._handleClickHamburger = this._handleClickHamburger.bind(this);
+    }
+
+    _handleClickHamburger() {
+        this.setState({ isDrawerOpen: true });
+    }
+
+    _handleToggleDrawer(state) {
+        this.setState({ isDrawerOpen: state });
     }
 
     render() {
+        const { isDrawerOpen } = this.state;
         return (
             <AppBar position="static" className="ib-header">
                 <Container>
@@ -30,34 +60,86 @@ class IdiotBoxHeader extends Component {
                         mode="horizontal"
                         className="ib-header-toolbar"
                     >
-                        <List
-                            component="nav"
-                            aria-labelledby="main navigation"
-                            className="linklist"
-                        >
-                            {navbarItems.map(({ title, path }) => (
+                        <Hidden mdUp>
+                            <IconButton
+                                edge="start"
+                                className="hamburger-button"
+                                onClick={this._handleClickHamburger}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography variant="h6">Idiot Box</Typography>
+                            <SwipeableDrawer
+                                open={isDrawerOpen}
+                                onClose={() => this._handleToggleDrawer(false)}
+                                onOpen={() => this._handleToggleDrawer(true)}
+                                className="ib-hamburger-drawer"
+                            >
+                                <List
+                                    component="nav"
+                                    aria-labelledby="main navigation"
+                                    className="drawer-linklist"
+                                    onClick={() =>
+                                        this._handleToggleDrawer(false)
+                                    }
+                                >
+                                    {navbarItems.map(
+                                        ({ title, path, icon }) => (
+                                            <Link key={path} to={path}>
+                                                <ListItem>
+                                                    <ListItemIcon>
+                                                        {icon}
+                                                    </ListItemIcon>
+                                                    <ListItemText
+                                                        primary={title}
+                                                    />
+                                                </ListItem>
+                                            </Link>
+                                        )
+                                    )}
+                                    <Link key={"/settings"} to="/settings">
+                                        <ListItem>
+                                            <ListItemIcon>
+                                                <SettingsIcon />
+                                            </ListItemIcon>
+                                            <ListItemText>
+                                                Settings
+                                            </ListItemText>
+                                        </ListItem>
+                                    </Link>
+                                </List>
+                            </SwipeableDrawer>
+                        </Hidden>
+                        <Hidden smDown>
+                            <List
+                                component="nav"
+                                aria-labelledby="main navigation"
+                                className="horizontal-linklist"
+                            >
+                                {navbarItems.map(({ title, path }) => (
+                                    <Link
+                                        key={path}
+                                        to={path}
+                                        className="linklist-linktext"
+                                    >
+                                        <ListItem button>
+                                            <ListItemText primary={title} />
+                                        </ListItem>
+                                    </Link>
+                                ))}
                                 <Link
-                                    key={path}
-                                    to={path}
-                                    className="linklist-linktext"
+                                    key={"/settings"}
+                                    to="/settings"
+                                    className="it-header-linktext"
                                 >
                                     <ListItem button>
-                                        <ListItemText primary={title} />
+                                        <ListItemIcon>
+                                            <SettingsIcon />
+                                        </ListItemIcon>
                                     </ListItem>
                                 </Link>
-                            ))}
-                            <Link
-                                key={"/settings"}
-                                to="/settings"
-                                className="it-header-linktext"
-                            >
-                                <ListItem button>
-                                    <ListItemIcon>
-                                        <SettingsIcon />
-                                    </ListItemIcon>
-                                </ListItem>
-                            </Link>
-                        </List>
+                            </List>
+                        </Hidden>
                     </Toolbar>
                 </Container>
             </AppBar>
