189a01e89eae56d4c35e53aa3a151139930660c8
Destin Moulton <destin.moulton@gmail.com>
2021-02-15 14:06:48 -0700

    Styled new episodes.

--- src/client/Components/NewEpisodes.js
+++ src/client/Components/NewEpisodes.js
@@ -5,9 +5,12 @@ import { Link } from "react-router-dom";
 import { truncate } from "lodash";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import EventAvailableIcon from "@material-ui/icons/EventAvailable";
+import EventBusyIcon from "@material-ui/icons/EventBusy";
+import Grid from "@material-ui/core/Grid";
 import { callAPI } from "../actions/api.actions";
 
+const NUM_DAYS = 14;
 class NewEpisodes extends Component {
     constructor(props) {
         super(props);
@@ -20,7 +23,7 @@ class NewEpisodes extends Component {
 
     componentWillMount() {
         let days = [];
-        for (let i = 0; i <= 7; i++) {
+        for (let i = 0; i <= NUM_DAYS; i++) {
             const dayMoment = moment().subtract(i, "day");
             days.push(dayMoment);
             this._getNewEpisodes(dayMoment);
@@ -80,56 +83,60 @@ class NewEpisodes extends Component {
             episode.show_info.title + " S" + sNum + "E" + eNum
         );
         return (
-            <div key={episode.id} className="ib-newepisode-container">
-                <div className="ib-newepisode-img-box">
-                    <Link to={"/show/" + episode.show_info.slug}>
-                        <img
-                            className="ib-newepisode-img"
-                            src={
-                                "/images/shows/" +
-                                episode.show_info.image_filename
-                            }
-                        />
-                    </Link>
-                </div>
-                <div className="ib-newepisode-details-box">
-                    <div className="ib-newepisode-show-title">
+            <Grid item xs={12} sm={6} lg={4} key={episode.id}>
+                <div className="ib-newepisode-container">
+                    <div className="ib-newepisode-img-box">
                         <Link to={"/show/" + episode.show_info.slug}>
-                            {truncate(episode.show_info.title, { length: 50 })}
+                            <img
+                                className="ib-newepisode-img"
+                                src={
+                                    "/images/shows/" +
+                                    episode.show_info.image_filename
+                                }
+                            />
                         </Link>
                     </div>
-                    <div className="ib-newepisode-ep-title">
-                        {truncate(episode.title, { length: 35 })}
-                    </div>
-                    <div>
-                        Season {episode.season_number} - Episode{" "}
-                        {episode.episode_number}
-                    </div>
-                    <div className="ib-newepisode-links">
-                        <a
-                            target="_blank"
-                            href={
-                                "https://www.1337x.to/search/" +
-                                searchString +
-                                "/1/"
-                            }
-                        >
-                            1337x
-                        </a>
-                        &nbsp;|&nbsp;
-                        <a
-                            target="_blank"
-                            href={
-                                "https://thepiratebay.org/search/" +
-                                searchString +
-                                "/0/99/0/"
-                            }
-                        >
-                            thepiratebay
-                        </a>
+                    <div className="ib-newepisode-details-box">
+                        <div className="ib-newepisode-show-title">
+                            <Link to={"/show/" + episode.show_info.slug}>
+                                {truncate(episode.show_info.title, {
+                                    length: 50,
+                                })}
+                            </Link>
+                        </div>
+                        <div className="ib-newepisode-ep-title">
+                            {truncate(episode.title, { length: 35 })}
+                        </div>
+                        <div>
+                            Season {episode.season_number} - Episode{" "}
+                            {episode.episode_number}
+                        </div>
+                        <div className="ib-newepisode-links">
+                            <a
+                                target="_blank"
+                                href={
+                                    "https://www.1337x.to/search/" +
+                                    searchString +
+                                    "/1/"
+                                }
+                            >
+                                1337x
+                            </a>
+                            &nbsp;|&nbsp;
+                            <a
+                                target="_blank"
+                                href={
+                                    "https://thepiratebay.org/search/" +
+                                    searchString +
+                                    "/0/99/0/"
+                                }
+                            >
+                                thepiratebay
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Grid>
         );
     }
 
@@ -139,34 +146,46 @@ class NewEpisodes extends Component {
         let elements = [];
         days.forEach((dayMoment) => {
             let unixDay = dayMoment.unix();
-            const header = (
-                <h3>
-                    <CalendarTodayIcon />
-                    &nbsp;{dayMoment.format("dddd, MMMM Do YYYY")}
-                </h3>
-            );
 
+            let headerIcon = null;
             let epList = [];
+            let noEpisodesClass = "";
             if (episodes.hasOwnProperty(unixDay)) {
                 if (episodes[unixDay].length === 0) {
-                    // No episodes message
-                    epList = (
-                        <div key={unixDay} className="ib-newepisode-container">
-                            <h3>Nothing to see here...</h3>
-                        </div>
+                    headerIcon = (
+                        <EventBusyIcon className="newepisodes-title-icon" />
                     );
+                    noEpisodesClass = "newepisodes-no-episodes";
+                    // No episodes message
+                    epList = <p>No new episodes.</p>;
                 } else {
+                    headerIcon = (
+                        <EventAvailableIcon className="newepisodes-title-icon" />
+                    );
                     episodes[unixDay].forEach((episode) => {
                         const epEl = this._buildEpisodeDetails(episode);
                         epList.push(epEl);
                     });
                 }
             }
+            const header = (
+                <h3>
+                    {headerIcon}
+                    <span className="newepisodes-title-text">
+                        {dayMoment.format("dddd, MMMM Do YYYY")}
+                    </span>
+                </h3>
+            );
             const el = (
-                <Card key={unixDay} className="ib-newepisode-section">
+                <Card
+                    key={unixDay}
+                    className={"ib-newepisodes-block " + noEpisodesClass}
+                >
                     <CardContent>
                         {header}
-                        {epList}
+                        <Grid container spacing={1}>
+                            {epList}
+                        </Grid>
                     </CardContent>
                 </Card>
             );
@@ -177,7 +196,12 @@ class NewEpisodes extends Component {
 
     render() {
         const episodesList = this._buildEpisodesList();
-        return <div>{episodesList}</div>;
+        return (
+            <div>
+                <h1>New Episodes</h1>
+                {episodesList}
+            </div>
+        );
     }
 }
 
