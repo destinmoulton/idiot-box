287b8ddf6b19e9ddf7963f7bf9777005d1405751
Destin Moulton <destin.moulton@gmail.com>
2021-02-15 10:22:30 -0700

    Fixed the style of the loading box.

--- src/client/Components/Layout/IdiotBoxLoading.js
+++ src/client/Components/Layout/IdiotBoxLoading.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 const IdiotBoxLoading = () => {
     return (
-        <div id="ib-loading-box">
-            <CircularProgress />
-            <br />
-            <br />
-            <span>Loading Idiot Box...</span>
-        </div>
+        <Grid container alignItems="center" justify="center">
+            <Grid item xs={8} className="ib-loading-box">
+                <CircularProgress />
+                <br />
+                <br />
+                <span>Loading Idiot Box...</span>
+            </Grid>
+        </Grid>
     );
 };
 
