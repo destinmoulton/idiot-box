a0ba8bb84959738f9e1f9cdcb9e4b8048fbd250c
Destin Moulton <5767076+destinmoulton@users.noreply.github.com>
2021-02-17 16:53:42 -0700

    Delete trakt.config.ts

--- src/server/config/trakt.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    client_id:
-        "ba4048ca75c0ddd7a343452fee0973708ce73d9dab06a88c64d06dfb42553ce7",
-    client_secret:
-        "70659d9857a6ad1b43373a03bea6c71cb9e5ba5cde2e1605802e65343e1a845d",
-};
