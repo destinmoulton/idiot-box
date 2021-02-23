8f75cdd1db892a9aa390deaf2044e2e85768e0ed
Destin Moulton <destin.moulton@gmail.com>
2021-02-23 10:55:02 -0700

    Removed config files

--- src/server/config/db.config.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export default {
-    filename: path.resolve(__dirname, '../../database/idiot-box.sqlite3'),
-    migrationsPath: path.resolve(__dirname, '../../migrations')
-};
--- src/server/config/thumbnails.config.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export default {
-    movies: path.resolve(__dirname, '../../public/images/movies'),
-    shows: path.resolve(__dirname, '../../public/images/shows')
-};
